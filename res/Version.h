1a03f972f4b804be3496f80bd641b8d578d2c592
crosire <crosiredev@gmail.com>
2014-09-15 16:15:42 +0200

    Added automatic release build versioning

---
+++ res/Version.h
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------
+  VERSION CONTROL BUILD SYSTEM                               
+  This header file was created by VERBUILD v1.0.1            
+  -----------------------------------------------------------
+  help : verbuild -?                                         
+  info : http://www.yeamaec.com                              
+         yeamaec@hanafos.com ,krkim@yeamaec.com              
+-----------------------------------------------------------*/
+
+#ifndef VERSIONNO__H
+#define VERSIONNO__H
+
+#define VERSION_FULL           0.0.0.0
+
+#define VERSION_BASEYEAR       0
+#define VERSION_DATE           "0000-00-00"
+#define VERSION_TIME           "00:00:00"
+
+#define VERSION_MAJOR          0
+#define VERSION_MINOR          0
+#define VERSION_BUILDNO        0
+#define VERSION_EXTEND         0
+
+#define VERSION_FILE           0,0,0,0
+#define VERSION_PRODUCT        0,0,0,0
+#define VERSION_FILESTR        "0,0,0,0"
+#define VERSION_PRODUCTSTR     "0,0,0,0"
+
+#endif
