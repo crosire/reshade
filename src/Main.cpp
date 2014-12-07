6ba70949e9f90fbabfbae41df7f6bf94b8b54e6e
crosire <crosiredev@gmail.com>
2014-12-07 01:16:44 +0100

    Added simple debugging countermeasures

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,16 +10,30 @@ _INITIALIZE_EASYLOGGINGPP;
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID lpvReserved)
 {
 	UNREFERENCED_PARAMETER(lpvReserved);
-	
+
+	#pragma region Anti Debugging
+#ifndef _DEBUG
+	OutputDebugString(TEXT("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s"));
+
+	char ollydbg[] = "effsnhm"; for (int i = 0; i < 7; ++i) ollydbg[i] ^= 42;
+	char windbg[] = "}CDnHMlXKGOiFKYY"; for (int i = 0; i < 16; ++i) windbg[i] ^= 42;
+
+	if (FindWindowA(ollydbg, nullptr) != nullptr || FindWindowA(windbg, nullptr) != nullptr)
+	{
+		ExitProcess(0);
+	}
+#endif
+	#pragma endregion
+
 	switch (fdwReason)
 	{
 		case DLL_PROCESS_ATTACH:
 		{
-			::DisableThreadLibraryCalls(hModule);
+			DisableThreadLibraryCalls(hModule);
 
 			TCHAR modulePath[MAX_PATH], executablePath[MAX_PATH];
-			::GetModuleFileName(hModule, modulePath, MAX_PATH);
-			::GetModuleFileName(nullptr, executablePath, MAX_PATH);
+			GetModuleFileName(hModule, modulePath, MAX_PATH);
+			GetModuleFileName(nullptr, executablePath, MAX_PATH);
 			
 			ReShade::Runtime::Startup(executablePath, modulePath);
 			break;
