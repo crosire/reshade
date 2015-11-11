98c9309f439071a764ead7e5c4145a4169ee70b8
crosire <crosiredev@gmail.com>
2015-11-11 11:33:05 +0100

    Removed WinSocks download hooks

--- source/Hooks/WS2_32.cpp
+++ source/Hooks/WS2_32.cpp
@@ -18,13 +18,6 @@ EXPORT int WSAAPI HookWSASend(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount,
 
 	return trampoline(s, lpBuffers, dwBufferCount, lpNumberOfBytesSent, dwFlags, lpOverlapped, lpCompletionRoutine);
 }
-EXPORT int WSAAPI HookXSASend(SOCKET s, const char *buf, int len, int flags)
-{
-	DWORD result = 0;
-	WSABUF buffer = { static_cast<ULONG>(len), const_cast<CHAR *>(buf) };
-
-	return HookWSASend(s, &buffer, 1, &result, flags, nullptr, nullptr) == 0 ? static_cast<int>(result) : SOCKET_ERROR;
-}
 EXPORT int WSAAPI HookWSASendTo(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesSent, DWORD dwFlags, const struct sockaddr *lpTo, int iToLen, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine)
 {
 	static const auto trampoline = ReShade::Hooks::Call(&HookWSASendTo);
@@ -36,60 +29,18 @@ EXPORT int WSAAPI HookWSASendTo(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCoun
 
 	return trampoline(s, lpBuffers, dwBufferCount, lpNumberOfBytesSent, dwFlags, lpTo, iToLen, lpOverlapped, lpCompletionRoutine);
 }
-EXPORT int WSAAPI HookXSASendTo(SOCKET s, const char *buf, int len, int flags, const struct sockaddr *to, int tolen)
+
+EXPORT int WSAAPI HookSend(SOCKET s, const char *buf, int len, int flags)
+{
+	DWORD result = 0;
+	WSABUF buffer = { static_cast<ULONG>(len), const_cast<CHAR *>(buf) };
+
+	return HookWSASend(s, &buffer, 1, &result, flags, nullptr, nullptr) == 0 ? static_cast<int>(result) : SOCKET_ERROR;
+}
+EXPORT int WSAAPI HookSendTo(SOCKET s, const char *buf, int len, int flags, const struct sockaddr *to, int tolen)
 {
 	DWORD result = 0;
 	WSABUF buffer = { static_cast<ULONG>(len), const_cast<CHAR *>(buf) };
 
 	return HookWSASendTo(s, &buffer, 1, &result, flags, to, tolen, nullptr, nullptr) == 0 ? static_cast<int>(result) : SOCKET_ERROR;
-}
-EXPORT int WSAAPI HookWSARecv(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesRecvd, LPDWORD lpFlags, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine)
-{
-	static const auto trampoline = ReShade::Hooks::Call(&HookWSARecv);
-
-	DWORD recieved = 0;
-
-	const auto status = trampoline(s, lpBuffers, dwBufferCount, &recieved, lpFlags, lpOverlapped, lpCompletionRoutine);
-
-	if (lpNumberOfBytesRecvd != nullptr)
-	{
-		*lpNumberOfBytesRecvd = recieved;
-	}
-
-	return status;
-}
-EXPORT int WSAAPI HookXSARecv(SOCKET s, char *buf, int len, int flags)
-{
-	DWORD result = 0, flags2 = flags;
-	WSABUF buffer = { static_cast<ULONG>(len), buf };
-
-	return HookWSARecv(s, &buffer, 1, &result, &flags2, nullptr, nullptr) == 0 ? static_cast<int>(result) : SOCKET_ERROR;
-}
-EXPORT int WSAAPI HookWSARecvEx(SOCKET s, char *buf, int len, int *flags)
-{
-	static const auto trampoline = ReShade::Hooks::Call(&HookWSARecvEx);
-
-	return trampoline(s, buf, len, flags);
-}
-EXPORT int WSAAPI HookWSARecvFrom(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesRecvd, LPDWORD lpFlags, struct sockaddr *lpFrom, LPINT lpFromlen, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine)
-{
-	static const auto trampoline = ReShade::Hooks::Call(&HookWSARecvFrom);
-
-	DWORD recieved = 0;
-
-	const auto status = trampoline(s, lpBuffers, dwBufferCount, &recieved, lpFlags, lpFrom, lpFromlen, lpOverlapped, lpCompletionRoutine);
-
-	if (lpNumberOfBytesRecvd != nullptr)
-	{
-		*lpNumberOfBytesRecvd = recieved;
-	}
-
-	return status;
-}
-EXPORT int WSAAPI HookXSARecvFrom(SOCKET s, char *buf, int len, int flags, struct sockaddr *from, int *fromlen)
-{
-	DWORD result = 0, flags2 = flags;
-	WSABUF buffer = { static_cast<ULONG>(len), buf };
-
-	return HookWSARecvFrom(s, &buffer, 1, &result, &flags2, from, fromlen, nullptr, nullptr) == 0 ? static_cast<int>(result) : SOCKET_ERROR;
 }
