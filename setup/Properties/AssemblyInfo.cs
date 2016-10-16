de315de2a57c22d877ffea1537b5dd12f8fdd2a1
crosire <crosiredev@gmail.com>
2016-10-16 14:21:25 +0200

    Remove file version from setup tool

--- setup/Properties/AssemblyInfo.cs
+++ setup/Properties/AssemblyInfo.cs
@@ -1,10 +1,9 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("ReShade Setup")]
+[assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("crosire")]
 [assembly: AssemblyProduct("ReShade")]
 [assembly: AssemblyCopyright("Copyright © 2014. All rights reserved.")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("3.0.2")]
