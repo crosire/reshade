c20abe7e7f32835e67e59a0de7284a0ea99fc399
crosire <crosiredev@gmail.com>
2018-04-14 19:31:05 +0200

    Fix buffer detection category being shown in non-D3D11 applications

--- source/runtime.cpp
+++ source/runtime.cpp
@@ -1793,7 +1793,7 @@ namespace reshade
 			}
 		}
 
-		const bool is_d3d11 = (_renderer_id & 0xb000) != 0;
+		const bool is_d3d11 = _renderer_id >= 0xb000 && _renderer_id < 0xc000;;
 
 		if (is_d3d11 && ImGui::CollapsingHeader("Buffer Detection", ImGuiTreeNodeFlags_DefaultOpen))
 		{
