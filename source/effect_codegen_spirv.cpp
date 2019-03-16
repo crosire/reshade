crosire/reshade

commit 41e651a6a68a0c1511b2eed76f6315a594891110

Rework index expressions in effect parser and clean up code

source/effect_codegen_spirv.cpp
@@ -1002,9 +1002,9 @@ private:
 		if (exp.is_constant) // Constant expressions do not have a complex access chain
 			return emit_constant(exp.type, exp.constant);
 
+		size_t i = 0;
 		spv::Id result = exp.base;
 
-		size_t op_index2 = 0;
 		auto base_type = exp.type;
 		bool is_uniform_bool = false;
 
@@ -1040,31 +1040,38 @@ private:
 			}
 
 			// Any indexing expressions can be resolved during load with an 'OpAccessChain' already
-			if (!exp.chain.empty() && (exp.chain[0].op == expression::operation::op_index || exp.chain[0].op == expression::operation::op_member))
+			if (!exp.chain.empty() && (
+				exp.chain[0].op == expression::operation::op_member ||
+				exp.chain[0].op == expression::operation::op_dynamic_index ||
+				exp.chain[0].op == expression::operation::op_constant_index))
 			{
 				spirv_instruction &node = add_instruction(spv::OpAccessChain)
 					.add(result); // Base
 
 				// Ignore first index into 1xN matrices, since they were translated to a vector type in SPIR-V
 				if (exp.chain[0].from.rows == 1 && exp.chain[0].from.cols > 1)
-					op_index2 = 1;
+					i = 1;
 
 				do {
-					base_type = exp.chain[op_index2].to;
-					if (exp.chain[op_index2].op == expression::operation::op_index)
-						node.add(exp.chain[op_index2++].index); // Indexes
-					else
-						node.add(emit_constant(exp.chain[op_index2++].index)); // Indexes
-				} while (op_index2 < exp.chain.size() && (exp.chain[op_index2].op == expression::operation::op_index || exp.chain[op_index2].op == expression::operation::op_member));
-				node.type = convert_type(exp.chain[op_index2 - 1].to, true, storage); // Last type is the result
-				result = node.result; // Result ID
+					node.add(exp.chain[i].op == expression::operation::op_dynamic_index ?
+						exp.chain[i].index :
+						emit_constant(exp.chain[i].index)); // Indexes
+					base_type = exp.chain[i++].to;
+				} while (i < exp.chain.size() && (
+					exp.chain[i].op == expression::operation::op_member ||
+					exp.chain[i].op == expression::operation::op_dynamic_index ||
+					exp.chain[i].op == expression::operation::op_constant_index));
+
+				node.type = convert_type(exp.chain[i - 1].to, true, storage); // Last type is the result
+				result = node.result;
 			}
 
 			result = add_instruction(spv::OpLoad, convert_type(base_type))
 				.add(result) // Pointer
-				.result; // Result ID
+				.result;
 		}
 
+		// Need to convert boolean uniforms which are actually integers in SPIR-V
 		if (is_uniform_bool)
 		{
 			base_type.base = type::t_bool;
@@ -1076,104 +1083,74 @@ private:
 		}
 
 		// Work through all remaining operations in the access chain and apply them to the value
-		for (; op_index2 < exp.chain.size(); ++op_index2)
+		for (; i < exp.chain.size(); ++i)
 		{
-			const auto &op = exp.chain[op_index2];
+			const auto &op = exp.chain[i];
 
 			switch (op.op)
 			{
 			case expression::operation::op_cast:
-				if (op.from.base != op.to.base)
+				if (op.from.is_boolean())
 				{
-					type from_with_to_base = op.from;
-					from_with_to_base.base = op.to.base;
+					const spv::Id true_constant = emit_constant(op.to, 1);
+					const spv::Id false_constant = emit_constant(op.to, 0);
 
-					if (op.from.is_boolean())
-					{
-						constant true_value = {};
-						constant false_value = {};
-						for (unsigned int i = 0; i < op.to.components(); ++i)
-							true_value.as_uint[i] = op.to.is_floating_point() ? 0x3f800000 : 1;
-						const spv::Id true_constant = emit_constant(from_with_to_base, true_value);
-						const spv::Id false_constant = emit_constant(from_with_to_base, false_value);
-
-						result = add_instruction(spv::OpSelect, convert_type(from_with_to_base))
-							.add(result) // Condition
-							.add(true_constant)
-							.add(false_constant)
-							.result;
-					}
-					else
-					{
-						switch (op.to.base)
-						{
-						case type::t_bool:
-							result = add_instruction(op.from.is_floating_point() ? spv::OpFOrdNotEqual : spv::OpINotEqual, convert_type(from_with_to_base))
-								.add(result)
-								.add(emit_constant(op.from, {}))
-								.result;
-							break;
-						case type::t_int:
-							result = add_instruction(op.from.is_floating_point() ? spv::OpConvertFToS : spv::OpBitcast, convert_type(from_with_to_base))
-								.add(result)
-								.result;
-							break;
-						case type::t_uint:
-							result = add_instruction(op.from.is_floating_point() ? spv::OpConvertFToU : spv::OpBitcast, convert_type(from_with_to_base))
-								.add(result)
-								.result;
-							break;
-						case type::t_float:
-							assert(op.from.is_integral());
-							result = add_instruction(op.from.is_signed() ? spv::OpConvertSToF : spv::OpConvertUToF, convert_type(from_with_to_base))
-								.add(result)
-								.result;
-							break;
-						}
-					}
+					result = add_instruction(spv::OpSelect, convert_type(op.to))
+						.add(result) // Condition
+						.add(true_constant)
+						.add(false_constant)
+						.result;
 				}
-
-				if (op.to.components() > op.from.components())
+				else if (op.to.is_boolean())
 				{
-					spirv_instruction &composite_node = add_instruction(exp.is_constant ? spv::OpConstantComposite : spv::OpCompositeConstruct, convert_type(op.to));
-					for (unsigned int i = 0; i < op.to.components(); ++i)
-						composite_node.add(result);
-					result = composite_node.result;
+					result = add_instruction(op.from.is_floating_point() ? spv::OpFOrdNotEqual : spv::OpINotEqual, convert_type(op.to))
+						.add(result)
+						.add(emit_constant(op.from, 0))
+						.result;
 				}
-				if (op.from.components() > op.to.components())
+				else
 				{
-					//signed char swizzle[4] = { -1, -1, -1, -1 };
-					//for (unsigned int i = 0; i < rhs.type.rows; ++i)
-					//	swizzle[i] = i;
-					//from.push_swizzle(swizzle);
-					assert(false); // TODO
+					spv::Op spv_op = spv::OpNop;
+
+					switch (op.to.base)
+					{
+					case type::t_int:
+						spv_op = op.from.is_floating_point() ? spv::OpConvertFToS : spv::OpBitcast;
+						break;
+					case type::t_uint:
+						spv_op = op.from.is_floating_point() ? spv::OpConvertFToU : spv::OpBitcast;
+						break;
+					case type::t_float:
+						assert(op.from.is_integral());
+						spv_op = op.from.is_signed() ? spv::OpConvertSToF : spv::OpConvertUToF;
+						break;
+					}
+
+					result = add_instruction(spv_op, convert_type(op.to))
+						.add(result)
+						.result;
 				}
 				break;
-			case expression::operation::op_index:
-				if (op.from.is_array())
+			case expression::operation::op_dynamic_index:
+				if (op.from.is_vector())
 				{
-					assert(false);
-					/*assert(result != 0);
-					//result = add_instruction(section, chain.location, spv::OpCompositeExtract, convert_type(op.to))
-					//	.add(result)
-					//	.add(op.index)
-					//	.result;
-					result = add_instruction(section, chain.location, spv::OpAccessChain, convert_type(op.to))
-						.add(result)
-						.add(op.index)
-						.result;
-					result = add_instruction(section, chain.location, spv::OpLoad, convert_type(op.to))
-						.add(result)
-						.result;*/
-					break;
-				}
-				else if (op.from.is_vector() && op.to.is_scalar())
-				{
-					assert(result != 0);
+					assert(result != 0 && op.to.is_scalar());
 					result = add_instruction(spv::OpVectorExtractDynamic, convert_type(op.to))
 						.add(result) // Vector
 						.add(op.index) // Index
 						.result; // Result ID
+					break;
+				}
+				assert(false);
+				break;
+			case expression::operation::op_constant_index:
+				if (op.from.is_vector())
+				{
+					assert(result != 0 && op.to.is_scalar());
+					result = add_instruction(spv::OpCompositeExtract, convert_type(op.to))
+						.add(result)
+						.add(op.index) // Literal Index
+						.result;
 					break;
 				}
 				assert(false);
@@ -1185,10 +1162,10 @@ private:
 					{
 						spv::Id components[4];
 
-						for (unsigned int i = 0; i < 4 && op.swizzle[i] >= 0; ++i)
+						for (unsigned int c = 0; c < 4 && op.swizzle[c] >= 0; ++c)
 						{
-							const unsigned int row = op.swizzle[i] / 4;
-							const unsigned int column = op.swizzle[i] - row * 4;
+							const unsigned int row = op.swizzle[c] / 4;
+							const unsigned int column = op.swizzle[c] - row * 4;
 
 							type scalar_type = op.to;
 							scalar_type.rows = 1;
@@ -1203,13 +1180,13 @@ private:
 
 							node.add(column);
 
-							components[i] = node.result;
+							components[c] = node.result;
 						}
 
 						spirv_instruction &node = add_instruction(spv::OpCompositeConstruct, convert_type(op.to));
 
-						for (unsigned int i = 0; i < 4 && op.swizzle[i] >= 0; ++i)
-							node.add(components[i]);
+						for (unsigned int c = 0; c < 4 && op.swizzle[c] >= 0; ++c)
+							node.add(components[c]);
 
 						result = node.result;
 						break;
@@ -1220,8 +1197,8 @@ private:
 							.add(result) // Vector 1
 							.add(result); // Vector 2
 
-						for (unsigned int i = 0; i < 4 && op.swizzle[i] >= 0; ++i)
-							node.add(op.swizzle[i]);
+						for (unsigned int c = 0; c < 4 && op.swizzle[c] >= 0; ++c)
+							node.add(op.swizzle[c]);
 
 						result = node.result;
 						break;
@@ -1230,22 +1207,20 @@ private:
 					{
 						spirv_instruction &node = add_instruction(spv::OpCompositeConstruct, convert_type(op.to));
 
-						for (unsigned int i = 0; i < op.to.rows; ++i)
+						for (unsigned int c = 0; c < op.to.rows; ++c)
 							node.add(result);
 
 						result = node.result;
 						break;
 					}
 				}
-				else if (op.to.is_scalar())
+				else if (op.from.is_matrix() && op.to.is_scalar())
 				{
-					assert(op.swizzle[1] < 0);
-
-					assert(result != 0);
+					assert(result != 0 && op.swizzle[1] < 0);
 					spirv_instruction &node = add_instruction(spv::OpCompositeExtract, convert_type(op.to))
 						.add(result); // Composite
 
-					if (op.from.is_matrix() && op.from.rows > 1)
+					if (op.from.rows > 1)
 					{
 						const unsigned int row = op.swizzle[0] / 4;
 						const unsigned int column = op.swizzle[0] - row * 4;
@@ -1274,16 +1249,16 @@ private:
 
 		add_location(exp.location, *_current_block_data);
 
+		size_t i = 0;
 		spv::Id target = exp.base;
 
-		size_t op_index2 = 0;
-
-		auto base_type = exp.type;
-		if (!exp.chain.empty())
-			base_type = exp.chain[0].from;
+		auto base_type = exp.chain.empty() ? exp.type : exp.chain[0].from;
 
 		// Any indexing expressions can be resolved with an 'OpAccessChain' already
-		if (!exp.chain.empty() && (exp.chain[0].op == expression::operation::op_index || exp.chain[0].op == expression::operation::op_member))
+		if (!exp.chain.empty() && (
+			exp.chain[0].op == expression::operation::op_member ||
+			exp.chain[0].op == expression::operation::op_dynamic_index ||
+			exp.chain[0].op == expression::operation::op_constant_index))
 		{
 			spv::StorageClass storage = spv::StorageClassFunction;
 			if (const auto it = _storage_lookup.find(exp.base); it != _storage_lookup.end())
@@ -1294,35 +1269,35 @@ private:
 
 			// Ignore first index into 1xN matrices, since they were translated to a vector type in SPIR-V
 			if (exp.chain[0].from.rows == 1 && exp.chain[0].from.cols > 1)
-				op_index2 = 1;
+				i = 1;
 
 			do {
-				base_type = exp.chain[op_index2].to;
-				if (exp.chain[op_index2].op == expression::operation::op_index)
-					node.add(exp.chain[op_index2++].index); // Indexes
-				else
-					node.add(emit_constant(exp.chain[op_index2++].index)); // Indexes
-			} while (op_index2 < exp.chain.size() && (exp.chain[op_index2].op == expression::operation::op_index || exp.chain[op_index2].op == expression::operation::op_member));
-			node.type = convert_type(exp.chain[op_index2 - 1].to, true, storage); // Last type is the result
-			target = node.result; // Result ID
+				node.add(exp.chain[i].op == expression::operation::op_dynamic_index ?
+					exp.chain[i].index :
+					emit_constant(exp.chain[i].index)); // Indexes
+				base_type = exp.chain[i++].to;
+			} while (i < exp.chain.size() && (
+				exp.chain[i].op == expression::operation::op_member ||
+				exp.chain[i].op == expression::operation::op_dynamic_index ||
+				exp.chain[i].op == expression::operation::op_constant_index));
+
+			node.type = convert_type(exp.chain[i - 1].to, true, storage); // Last type is the result
+			target = node.result;
 		}
 
 		// TODO: Complex access chains like float4x4[0].m00m10[0] = 0;
 		// Work through all remaining operations in the access chain and apply them to the value
-		for (; op_index2 < exp.chain.size(); ++op_index2)
+		for (; i < exp.chain.size(); ++i)
 		{
-			const auto &op = exp.chain[op_index2];
+			const auto &op = exp.chain[i];
 
 			switch (op.op)
 			{
-			case expression::operation::op_cast:
-				assert(false); // This cannot happen
-				break;
-			case expression::operation::op_index:
+			case expression::operation::op_constant_index:
+			case expression::operation::op_dynamic_index:
 				assert(false);
 				break;
-			case expression::operation::op_swizzle:
-			{
+			case expression::operation::op_swizzle: {
 				spv::Id result = add_instruction(spv::OpLoad, convert_type(base_type))
 					.add(target) // Pointer
 					.result; // Result ID
@@ -1334,17 +1309,17 @@ private:
 						.add(value); // Vector 2
 
 					unsigned int shuffle[4] = { 0, 1, 2, 3 };
-					for (unsigned int i = 0; i < base_type.rows; ++i)
-						if (op.swizzle[i] >= 0)
-							shuffle[op.swizzle[i]] = base_type.rows + i;
-					for (unsigned int i = 0; i < base_type.rows; ++i)
-						node.add(shuffle[i]);
+					for (unsigned int c = 0; c < base_type.rows; ++c)
+						if (op.swizzle[c] >= 0)
+							shuffle[op.swizzle[c]] = base_type.rows + i;
+					for (unsigned int c = 0; c < base_type.rows; ++c)
+						node.add(shuffle[c]);
 
 					value = node.result;
 				}
 				else if (op.to.is_scalar())
 				{
-					assert(op.swizzle[1] < 0); // TODO
+					assert(op.swizzle[1] < 0);
 
 					spirv_instruction &node = add_instruction(spv::OpCompositeInsert, convert_type(base_type))
 						.add(value) // Object
@@ -1368,8 +1343,7 @@ private:
 				{
 					assert(false);
 				}
-				break;
-			}
+				break; }
 			}
 		}
 
@@ -1383,6 +1357,16 @@ private:
 		constant data;
 		data.as_uint[0] = value;
 		return emit_constant({ type::t_uint, 1, 1 }, data, false);
+	}
+	id   emit_constant(const type &type, uint32_t value)
+	{
+		constant data;
+		for (unsigned int i = 0; i < type.components(); ++i)
+			if (type.is_integral())
+				data.as_uint[i] = value;
+			else
+				data.as_float[i] = static_cast<float>(value);
+		return emit_constant(type, data, false);
 	}
 	id   emit_constant(const type &type, const constant &data) override
 	{

source/effect_codegen_glsl.cpp
@@ -664,12 +664,15 @@ private:
 				{ std::string type; write_type<false, false>(type, op.to);
 				newcode = type + '(' + newcode + ')'; }
 				break;
-			case expression::operation::op_index:
-				newcode += '[' + id_to_name(op.index) + ']';
-				break;
 			case expression::operation::op_member:
 				newcode += '.';
 				newcode += find_struct(op.from.definition).member_list[op.index].name;
+				break;
+			case expression::operation::op_dynamic_index:
+				newcode += '[' + id_to_name(op.index) + ']';
+				break;
+			case expression::operation::op_constant_index:
+				newcode += '[' + std::to_string(op.index) + ']';
 				break;
 			case expression::operation::op_swizzle:
 				if (op.from.is_matrix())
@@ -683,6 +686,7 @@ private:
 					}
 					else
 					{
+						// TODO: Implement matrix to vector swizzles
 						assert(false);
 					}
 				}
@@ -720,12 +724,15 @@ private:
 		{
 			switch (op.op)
 			{
-			case expression::operation::op_index:
-				code += '[' + id_to_name(op.index) + ']';
-				break;
 			case expression::operation::op_member:
 				code += '.';
 				code += find_struct(op.from.definition).member_list[op.index].name;
+				break;
+			case expression::operation::op_dynamic_index:
+				code += '[' + id_to_name(op.index) + ']';
+				break;
+			case expression::operation::op_constant_index:
+				code += '[' + std::to_string(op.index) + ']';
 				break;
 			case expression::operation::op_swizzle:
 				if (op.from.is_matrix())
@@ -739,6 +746,7 @@ private:
 					}
 					else
 					{
+						// TODO: Implement matrix to vector swizzles
 						assert(false);
 					}
 				}

source/effect_codegen_hlsl.cpp
@@ -664,12 +664,15 @@ private:
 				{ std::string type; write_type<false, false>(type, op.to);
 				newcode = "((" + type + ')' + newcode + ')'; }
 				break;
-			case expression::operation::op_index:
-				newcode += '[' + id_to_name(op.index) + ']';
-				break;
 			case expression::operation::op_member:
 				newcode += '.';
 				newcode += find_struct(op.from.definition).member_list[op.index].name;
+				break;
+			case expression::operation::op_constant_index:
+				newcode += '[' + std::to_string(op.index) + ']';
+				break;
+			case expression::operation::op_dynamic_index:
+				newcode += '[' + id_to_name(op.index) + ']';
 				break;
 			case expression::operation::op_swizzle:
 				newcode += '.';
@@ -706,12 +709,15 @@ private:
 		{
 			switch (op.op)
 			{
-			case expression::operation::op_index:
-				code += '[' + id_to_name(op.index) + ']';
-				break;
 			case expression::operation::op_member:
 				code += '.';
 				code += find_struct(op.from.definition).member_list[op.index].name;
+				break;
+			case expression::operation::op_dynamic_index:
+				code += '[' + id_to_name(op.index) + ']';
+				break;
+			case expression::operation::op_constant_index:
+				code += '[' + std::to_string(op.index) + ']';
 				break;
 			case expression::operation::op_swizzle:
 				code += '.';

source/effect_expression.cpp
@@ -161,51 +161,18 @@ void reshadefx::expression::add_cast_operation(const reshadefx::type &cast_type)
 }
 void reshadefx::expression::add_member_access(unsigned int index, const reshadefx::type &in_type)
 {
-	const auto prev_type = type;
+	assert(type.is_struct());
 
+	chain.push_back({ operation::op_member, type, in_type, index });
+
+	// The type is now the type of the member that was accessed
 	type = in_type;
 	is_constant = false;
-
-	chain.push_back({ operation::op_member, prev_type, type, index });
 }
-void reshadefx::expression::add_static_index_access(reshadefx::codegen *codegen, unsigned int index)
+void reshadefx::expression::add_dynamic_index_access(reshadefx::codegen::id index_expression)
 {
-	// A static index into a non-constant expression is functionally equivalent to a dynamic index
-	if (!is_constant)
-	{
-		constant.as_uint[0] = index;
+	assert(type.is_numeric() && !is_constant);
 
-		return add_dynamic_index_access(codegen, codegen->emit_constant({ type::t_uint, 1, 1 }, constant));
-	}
-
-	if (type.is_array())
-	{
-		type.array_length = 0;
-
-		constant = constant.array_data[index];
-	}
-	else if (type.is_matrix()) // Indexing into a matrix returns a row of it as a vector
-	{
-		type.rows = type.cols;
-		type.cols = 1;
-
-		for (unsigned int i = 0; i < 4; ++i)
-			constant.as_uint[i] = constant.as_uint[index * 4 + i];
-	}
-	else if (type.is_vector()) // Indexing into a vector returns the element as a scalar
-	{
-		type.rows = 1;
-
-		constant.as_uint[0] = constant.as_uint[index];
-	}
-	else
-	{
-		// There is no other type possible which allows indexing
-		assert(false);
-	}
-}
-void reshadefx::expression::add_dynamic_index_access(reshadefx::codegen *codegen, reshadefx::codegen::id index_expression)
-{
 	auto prev_type = type;
 
 	if (type.is_array())
@@ -222,21 +189,53 @@ void reshadefx::expression::add_dynamic_index_access(reshadefx::codegen *codegen
 		type.rows = 1;
 	}
 
-	// To handle a dynamic index into a constant means we need to create a local variable first or else any of the indexing instructions do not work
-	if (is_constant)
-	{
-		base = codegen->define_variable(location, prev_type, std::string(), false, codegen->emit_constant(prev_type, constant));
+	chain.push_back({ operation::op_dynamic_index, prev_type, type, index_expression });
+}
+void reshadefx::expression::add_constant_index_access(unsigned int index)
+{
+	assert(type.is_numeric() && !type.is_scalar());
 
-		// We converted the constant to a l-value reference to a new variable
-		is_lvalue = true;
+	auto prev_type = type;
+
+	if (type.is_array())
+	{
+		type.array_length = 0;
+	}
+	else if (type.is_matrix())
+	{
+		type.rows = type.cols;
+		type.cols = 1;
+	}
+	else if (type.is_vector())
+	{
+		type.rows = 1;
 	}
 
-	chain.push_back({ operation::op_index, prev_type, type, index_expression });
-
-	is_constant = false;
+	if (is_constant)
+	{
+		if (prev_type.is_array())
+		{
+			constant = constant.array_data[index];
+		}
+		else if (prev_type.is_matrix()) // Indexing into a matrix returns a row of it as a vector
+		{
+			for (unsigned int i = 0; i < 4; ++i)
+				constant.as_uint[i] = constant.as_uint[index * 4 + i];
+		}
+		else // Indexing into a vector returns the element as a scalar
+		{
+			constant.as_uint[0] = constant.as_uint[index];
+		}
+	}
+	else
+	{
+		chain.push_back({ operation::op_constant_index, prev_type, type, index });
+	}
 }
-void reshadefx::expression::add_swizzle_access(const signed char in_swizzle[4], unsigned int length)
+void reshadefx::expression::add_swizzle_access(const signed char swizzle[4], unsigned int length)
 {
+	assert(type.is_numeric() && !type.is_array());
+
 	const auto prev_type = type;
 
 	type.rows = length;
@@ -249,18 +248,23 @@ void reshadefx::expression::add_swizzle_access(const signed char in_swizzle[4], 
 		uint32_t data[16];
 		memcpy(data, &constant.as_uint[0], sizeof(data));
 		for (unsigned int i = 0; i < length; ++i)
-			constant.as_uint[i] = data[in_swizzle[i]];
+			constant.as_uint[i] = data[swizzle[i]];
 		memset(&constant.as_uint[length], 0, sizeof(uint32_t) * (16 - length)); // Clear the rest of the constant
+	}
+	else if (length == 1 && prev_type.is_vector()) // Use indexing when possible since the code generation logic is simpler in SPIR-V
+	{
+		chain.push_back({ operation::op_constant_index, prev_type, type, static_cast<uint32_t>(swizzle[0]) });
 	}
 	else
 	{
-		chain.push_back({ operation::op_swizzle, prev_type, type, 0, { in_swizzle[0], in_swizzle[1], in_swizzle[2], in_swizzle[3] } });
+		chain.push_back({ operation::op_swizzle, prev_type, type, 0, { swizzle[0], swizzle[1], swizzle[2], swizzle[3] } });
 	}
 }
 
-void reshadefx::expression::evaluate_constant_expression(reshadefx::tokenid op)
+bool reshadefx::expression::evaluate_constant_expression(reshadefx::tokenid op)
 {
-	assert(is_constant);
+	if (!is_constant)
+		return false;
 
 	switch (op)
 	{
@@ -281,10 +285,13 @@ void reshadefx::expression::evaluate_constant_expression(reshadefx::tokenid op)
 			constant.as_uint[i] = ~constant.as_uint[i];
 		break;
 	}
+
+	return true;
 }
-void reshadefx::expression::evaluate_constant_expression(reshadefx::tokenid op, const reshadefx::constant &rhs)
+bool reshadefx::expression::evaluate_constant_expression(reshadefx::tokenid op, const reshadefx::constant &rhs)
 {
-	assert(is_constant);
+	if (!is_constant)
+		return false;
 
 	switch (op)
 	{
@@ -433,4 +440,6 @@ void reshadefx::expression::evaluate_constant_expression(reshadefx::tokenid op, 
 				constant.as_uint[i] >>= rhs.as_uint[i];
 		break;
 	}
+
+	return true;
 }

source/effect_expression.hpp
@@ -132,8 +132,9 @@ namespace reshadefx
 			enum op_type
 			{
 				op_cast,
-				op_index,
 				op_member,
+				op_dynamic_index,
+				op_constant_index,
 				op_swizzle,
 			};
 
@@ -190,12 +191,15 @@ namespace reshadefx
 		/// <param name="type">The value type of the member.</param>
 		void add_member_access(unsigned int index, const reshadefx::type &type);
 		/// <summary>
-		/// 
+		/// Add an index operation to the current access chain.
 		/// </summary>
-		/// <param name="codegen"></param>
-		/// <param name="index"></param>
-		void add_static_index_access(class codegen *codegen, unsigned int index);
-		void add_dynamic_index_access(class codegen *codegen, uint32_t index_expression);
+		/// <param name="index_expression">The SSA ID of the indexing value.</param>
+		void add_dynamic_index_access(uint32_t index_expression);
+		/// <summary>
+		/// Add an constant index operation to the current access chain.
+		/// </summary>
+		/// <param name="index">The constant indexing value.</param>
+		void add_constant_index_access(unsigned int index);
 		/// <summary>
 		/// Add a swizzle operation to the current access chain.
 		/// </summary>
@@ -207,13 +211,13 @@ namespace reshadefx
 		/// Apply an unary operation to this constant expression.
 		/// </summary>
 		/// <param name="op">The unary operator to apply.</param>
-		void evaluate_constant_expression(enum class tokenid op);
+		bool evaluate_constant_expression(enum class tokenid op);
 		/// <summary>
 		/// Apply a binary operation to this constant expression.
 		/// </summary>
 		/// <param name="op">The binary operator to apply.</param>
 		/// <param name="rhs">The constant to use as right-hand side of the binary operation.</param>
-		void evaluate_constant_expression(enum class tokenid op, const reshadefx::constant &rhs);
+		bool evaluate_constant_expression(enum class tokenid op, const reshadefx::constant &rhs);
 	};
 
 

commit bfa7c48584a7ab535306e62d9114c06604bb7633

Fix invalid SPIR-V code generation for storage swizzles
´
Bug was introduced during renaming in commit 41e651a6a68a0c1511b2eed76f6315a594891110

source/effect_codegen_spirv.cpp
@@ -1311,7 +1311,7 @@ private:
 					unsigned int shuffle[4] = { 0, 1, 2, 3 };
 					for (unsigned int c = 0; c < base_type.rows; ++c)
 						if (op.swizzle[c] >= 0)
-							shuffle[op.swizzle[c]] = base_type.rows + i;
+							shuffle[op.swizzle[c]] = base_type.rows + c;
 					for (unsigned int c = 0; c < base_type.rows; ++c)
 						node.add(shuffle[c]);
 