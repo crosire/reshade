crosire/reshade

commit 6c558f46d14412b31197abf3af6cfe2af857bddc

Clean up code

source/d3d10/runtime_d3d10.hpp
@@ -9,39 +9,10 @@
 #include "state_block.hpp"
 #include "draw_call_tracker.hpp"
 
+namespace reshadefx { struct sampler_info; }
+
 namespace reshade::d3d10
 {
-	struct d3d10_tex_data : base_object
-	{
-		com_ptr<ID3D10Texture2D> texture;
-		com_ptr<ID3D10ShaderResourceView> srv[2];
-		com_ptr<ID3D10RenderTargetView> rtv[2];
-	};
-	struct d3d10_pass_data : base_object
-	{
-		com_ptr<ID3D10VertexShader> vertex_shader;
-		com_ptr<ID3D10PixelShader> pixel_shader;
-		com_ptr<ID3D10BlendState> blend_state;
-		com_ptr<ID3D10DepthStencilState> depth_stencil_state;
-		UINT stencil_reference;
-		bool clear_render_targets;
-		com_ptr<ID3D10RenderTargetView> render_targets[D3D10_SIMULTANEOUS_RENDER_TARGET_COUNT];
-		com_ptr<ID3D10ShaderResourceView> render_target_resources[D3D10_SIMULTANEOUS_RENDER_TARGET_COUNT];
-		D3D10_VIEWPORT viewport;
-		std::vector<com_ptr<ID3D10ShaderResourceView>> shader_resources;
-	};
-	struct d3d10_technique_data : base_object
-	{
-		bool query_in_flight = false;
-		com_ptr<ID3D10Query> timestamp_disjoint;
-		com_ptr<ID3D10Query> timestamp_query_beg;
-		com_ptr<ID3D10Query> timestamp_query_end;
-		std::vector<com_ptr<ID3D10SamplerState>> sampler_states;
-		std::vector<com_ptr<ID3D10ShaderResourceView>> texture_bindings;
-		ptrdiff_t uniform_storage_offset = 0;
-		ptrdiff_t uniform_storage_index = -1;
-	};
-
 	class runtime_d3d10 : public runtime
 	{
 	public:
@@ -51,16 +22,8 @@ namespace reshade::d3d10
 		bool on_init(const DXGI_SWAP_CHAIN_DESC &desc);
 		void on_reset();
 		void on_present(draw_call_tracker& tracker);
-		void on_copy_resource(ID3D10Resource *&dest, ID3D10Resource *&source);
 
-		void capture_frame(uint8_t *buffer) const override;
-		void update_texture(texture &texture, const uint8_t *data) override;
-		bool update_texture_reference(texture &texture);
-
-		bool compile_effect(effect_data &effect) override;
-		void unload_effects() override;
-
-		void render_technique(const technique &technique) override;
+		void capture_screenshot(uint8_t *buffer) const override;
 
 #if RESHADE_DX10_CAPTURE_DEPTH_BUFFERS
 		com_ptr<ID3D10Texture2D> select_depth_texture_save(D3D10_TEXTURE2D_DESC &texture_desc);
@@ -71,7 +34,8 @@ namespace reshade::d3d10
 
 		com_ptr<ID3D10Texture2D> _backbuffer_texture;
 		com_ptr<ID3D10RenderTargetView> _backbuffer_rtv[3];
-		com_ptr<ID3D10ShaderResourceView> _backbuffer_texture_srv[2], _depthstencil_texture_srv;
+		com_ptr<ID3D10ShaderResourceView> _backbuffer_texture_srv[2];
+		com_ptr<ID3D10ShaderResourceView> _depthstencil_texture_srv;
 		std::unordered_map<size_t, com_ptr<ID3D10SamplerState>> _effect_sampler_states;
 		std::vector<com_ptr<ID3D10Buffer>> _constant_buffers;
 
@@ -81,16 +45,20 @@ namespace reshade::d3d10
 		int depth_buffer_texture_format = 0; // No depth buffer texture format filter by default
 
 	private:
-		int _depth_buffer_texture_format = 0;
-		unsigned int _selected_depth_buffer_texture_index = 0;
-
 		bool init_backbuffer_texture();
 		bool init_default_depth_stencil();
-		bool init_fx_resources();
 
-		bool add_sampler(const reshadefx::sampler_info &info, d3d10_technique_data &technique_init);
 		bool init_texture(texture &info) override;
-		bool init_technique(technique &info, const d3d10_technique_data &technique_init, const std::unordered_map<std::string, com_ptr<ID3D10VertexShader>> &vs_entry_points, const std::unordered_map<std::string, com_ptr<ID3D10PixelShader>> &ps_entry_points);
+		void update_texture(texture &texture, const uint8_t *pixels) override;
+		bool update_texture_reference(texture &texture);
+
+		bool compile_effect(effect_data &effect) override;
+		void unload_effects() override;
+
+		bool add_sampler(const reshadefx::sampler_info &info, struct d3d10_technique_data &technique_init);
+		bool init_technique(technique &info, struct d3d10_technique_data &&technique_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points);
+
+		void render_technique(const technique &technique) override;
 
 #if RESHADE_GUI
 		bool init_imgui_resources();
@@ -116,7 +84,7 @@ namespace reshade::d3d10
 
 		bool _is_multisampling_enabled = false;
 		DXGI_FORMAT _backbuffer_format = DXGI_FORMAT_UNKNOWN;
-		state_block _stateblock;
+		state_block _app_state;
 		com_ptr<ID3D10Texture2D> _backbuffer, _backbuffer_resolved;
 		com_ptr<ID3D10DepthStencilView> _depthstencil, _depthstencil_replacement;
 		ID3D10DepthStencilView *_best_depth_stencil_overwrite = nullptr;
@@ -127,7 +95,10 @@ namespace reshade::d3d10
 		com_ptr<ID3D10SamplerState> _copy_sampler;
 		com_ptr<ID3D10RasterizerState> _effect_rasterizer_state;
 
-		com_ptr<ID3D10Buffer> _imgui_vertex_buffer, _imgui_index_buffer;
+		int _imgui_index_buffer_size = 0;
+		com_ptr<ID3D10Buffer> _imgui_index_buffer;
+		int _imgui_vertex_buffer_size = 0;
+		com_ptr<ID3D10Buffer> _imgui_vertex_buffer;
 		com_ptr<ID3D10VertexShader> _imgui_vertex_shader;
 		com_ptr<ID3D10PixelShader> _imgui_pixel_shader;
 		com_ptr<ID3D10InputLayout> _imgui_input_layout;
@@ -136,8 +107,7 @@ namespace reshade::d3d10
 		com_ptr<ID3D10RasterizerState> _imgui_rasterizer_state;
 		com_ptr<ID3D10BlendState> _imgui_blend_state;
 		com_ptr<ID3D10DepthStencilState> _imgui_depthstencil_state;
-		int _imgui_vertex_buffer_size = 0, _imgui_index_buffer_size = 0;
-		draw_call_tracker _current_tracker;
+		draw_call_tracker *_current_tracker = nullptr;
 
 		HMODULE _d3d_compiler = nullptr;
 	};

source/d3d11/runtime_d3d11.hpp
@@ -10,47 +10,10 @@
 #include "draw_call_tracker.hpp"
 #include <mutex>
 
-inline reshade::log::message &operator<<(reshade::log::message &m, REFIID riid)
-{
-	OLECHAR riid_string[40];
-	StringFromGUID2(riid, riid_string, ARRAYSIZE(riid_string));
-
-	return m << riid_string;
-}
+namespace reshadefx { struct sampler_info; }
 
 namespace reshade::d3d11
 {
-	struct d3d11_tex_data : base_object
-	{
-		com_ptr<ID3D11Texture2D> texture;
-		com_ptr<ID3D11ShaderResourceView> srv[2];
-		com_ptr<ID3D11RenderTargetView> rtv[2];
-	};
-	struct d3d11_pass_data : base_object
-	{
-		com_ptr<ID3D11VertexShader> vertex_shader;
-		com_ptr<ID3D11PixelShader> pixel_shader;
-		com_ptr<ID3D11BlendState> blend_state;
-		com_ptr<ID3D11DepthStencilState> depth_stencil_state;
-		UINT stencil_reference;
-		bool clear_render_targets;
-		com_ptr<ID3D11RenderTargetView> render_targets[D3D11_SIMULTANEOUS_RENDER_TARGET_COUNT];
-		com_ptr<ID3D11ShaderResourceView> render_target_resources[D3D11_SIMULTANEOUS_RENDER_TARGET_COUNT];
-		D3D11_VIEWPORT viewport;
-		std::vector<com_ptr<ID3D11ShaderResourceView>> shader_resources;
-	};
-	struct d3d11_technique_data : base_object
-	{
-		bool query_in_flight = false;
-		com_ptr<ID3D11Query> timestamp_disjoint;
-		com_ptr<ID3D11Query> timestamp_query_beg;
-		com_ptr<ID3D11Query> timestamp_query_end;
-		std::vector<com_ptr<ID3D11SamplerState>> sampler_states;
-		std::vector<com_ptr<ID3D11ShaderResourceView>> texture_bindings;
-		ptrdiff_t uniform_storage_offset = 0;
-		ptrdiff_t uniform_storage_index = -1;
-	};
-
 	class runtime_d3d11 : public runtime
 	{
 	public:
@@ -61,14 +24,7 @@ namespace reshade::d3d11
 		void on_reset();
 		void on_present(draw_call_tracker& tracker);
 
-		void capture_frame(uint8_t *buffer) const override;
-		void update_texture(texture &texture, const uint8_t *data) override;
-		bool update_texture_reference(texture &texture);
-
-		bool compile_effect(effect_data &effect) override;
-		void unload_effects() override;
-
-		void render_technique(const technique &technique) override;
+		void capture_screenshot(uint8_t *buffer) const override;
 
 #if RESHADE_DX11_CAPTURE_DEPTH_BUFFERS
 		com_ptr<ID3D11Texture2D> select_depth_texture_save(D3D11_TEXTURE2D_DESC &texture_desc);
@@ -79,8 +35,8 @@ namespace reshade::d3d11
 		com_ptr<IDXGISwapChain> _swapchain;
 
 		com_ptr<ID3D11Texture2D> _backbuffer_texture;
-		com_ptr<ID3D11ShaderResourceView> _backbuffer_texture_srv[2];
 		com_ptr<ID3D11RenderTargetView> _backbuffer_rtv[3];
+		com_ptr<ID3D11ShaderResourceView> _backbuffer_texture_srv[2];
 		com_ptr<ID3D11ShaderResourceView> _depthstencil_texture_srv;
 		std::unordered_map<size_t, com_ptr<ID3D11SamplerState>> _effect_sampler_states;
 		std::vector<com_ptr<ID3D11Buffer>> _constant_buffers;
@@ -93,11 +49,18 @@ namespace reshade::d3d11
 	private:
 		bool init_backbuffer_texture();
 		bool init_default_depth_stencil();
-		bool init_fx_resources();
 
-		bool add_sampler(const reshadefx::sampler_info &info, d3d11_technique_data &technique_init);
 		bool init_texture(texture &info) override;
-		bool init_technique(technique &info, const d3d11_technique_data &technique_init, const std::unordered_map<std::string, com_ptr<ID3D11VertexShader>> &vs_entry_points, const std::unordered_map<std::string, com_ptr<ID3D11PixelShader>> &ps_entry_points);
+		void update_texture(texture &texture, const uint8_t *pixels) override;
+		bool update_texture_reference(texture &texture);
+
+		bool compile_effect(effect_data &effect) override;
+		void unload_effects() override;
+
+		bool add_sampler(const reshadefx::sampler_info &info, struct d3d11_technique_data &technique_init);
+		bool init_technique(technique &info, struct d3d11_technique_data &&technique_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points);
+
+		void render_technique(const technique &technique) override;
 
 #if RESHADE_GUI
 		bool init_imgui_resources();
@@ -123,7 +86,7 @@ namespace reshade::d3d11
 
 		bool _is_multisampling_enabled = false;
 		DXGI_FORMAT _backbuffer_format = DXGI_FORMAT_UNKNOWN;
-		state_block _stateblock;
+		state_block _app_state;
 		com_ptr<ID3D11Texture2D> _backbuffer, _backbuffer_resolved;
 		com_ptr<ID3D11DepthStencilView> _depthstencil, _depthstencil_replacement;
 		ID3D11DepthStencilView *_best_depth_stencil_overwrite = nullptr;
@@ -135,7 +98,10 @@ namespace reshade::d3d11
 		std::mutex _mutex;
 		com_ptr<ID3D11RasterizerState> _effect_rasterizer_state;
 
-		com_ptr<ID3D11Buffer> _imgui_vertex_buffer, _imgui_index_buffer;
+		int _imgui_index_buffer_size = 0;
+		com_ptr<ID3D11Buffer> _imgui_index_buffer;
+		int _imgui_vertex_buffer_size = 0;
+		com_ptr<ID3D11Buffer> _imgui_vertex_buffer;
 		com_ptr<ID3D11VertexShader> _imgui_vertex_shader;
 		com_ptr<ID3D11PixelShader> _imgui_pixel_shader;
 		com_ptr<ID3D11InputLayout> _imgui_input_layout;
@@ -144,8 +110,7 @@ namespace reshade::d3d11
 		com_ptr<ID3D11RasterizerState> _imgui_rasterizer_state;
 		com_ptr<ID3D11BlendState> _imgui_blend_state;
 		com_ptr<ID3D11DepthStencilState> _imgui_depthstencil_state;
-		int _imgui_vertex_buffer_size = 0, _imgui_index_buffer_size = 0;
-		draw_call_tracker _current_tracker;
+		draw_call_tracker *_current_tracker = nullptr;
 
 		HMODULE _d3d_compiler = nullptr;
 	};

source/d3d9/runtime_d3d9.hpp
@@ -7,34 +7,12 @@
 
 #include "runtime.hpp"
 #include "effect_expression.hpp"
-#include "com_ptr.hpp"
-#include <d3d9.h>
+#include "state_block.hpp"
+
+namespace reshadefx { struct sampler_info; }
 
 namespace reshade::d3d9
 {
-	struct d3d9_tex_data : base_object
-	{
-		com_ptr<IDirect3DTexture9> texture;
-		com_ptr<IDirect3DSurface9> surface;
-	};
-	struct d3d9_pass_data : base_object
-	{
-		com_ptr<IDirect3DVertexShader9> vertex_shader;
-		com_ptr<IDirect3DPixelShader9> pixel_shader;
-		com_ptr<IDirect3DStateBlock9> stateblock;
-		bool clear_render_targets = false;
-		IDirect3DSurface9 *render_targets[8] = {};
-		IDirect3DTexture9 *sampler_textures[16] = {};
-	};
-	struct d3d9_technique_data : base_object
-	{
-		DWORD num_samplers = 0;
-		DWORD sampler_states[16][12] = {};
-		IDirect3DTexture9 *sampler_textures[16] = {};
-		DWORD constant_register_count = 0;
-		size_t uniform_storage_offset = 0;
-	};
-
 	class runtime_d3d9 : public runtime
 	{
 	public:
@@ -44,18 +22,13 @@ namespace reshade::d3d9
 		bool on_init(const D3DPRESENT_PARAMETERS &pp);
 		void on_reset();
 		void on_present();
-		void on_draw_call(D3DPRIMITIVETYPE type, UINT count);
+
+		void on_draw_call(D3DPRIMITIVETYPE type, unsigned int count);
 		void on_set_depthstencil_surface(IDirect3DSurface9 *&depthstencil);
 		void on_get_depthstencil_surface(IDirect3DSurface9 *&depthstencil);
-		bool on_clear(com_ptr<IDirect3DSurface9> depthstencil);
+		void on_clear_depthstencil_surface(IDirect3DSurface9 *depthstencil);
 
-		void capture_frame(uint8_t *buffer) const override;
-		void update_texture(texture &texture, const uint8_t *data) override;
-		bool update_texture_reference(texture &texture);
-
-		bool compile_effect(effect_data &effect) override;
-
-		void render_technique(const technique &technique) override;
+		void capture_screenshot(uint8_t *buffer) const override;
 
 		com_ptr<IDirect3D9> _d3d;
 		com_ptr<IDirect3DDevice9> _device;
@@ -70,12 +43,6 @@ namespace reshade::d3d9
 	private:
 		struct depth_source_info
 		{
-			UINT width, height;
-			UINT drawcall_count, vertices_count;
-		};
-
-		struct depth_buffer_info
-		{
 			com_ptr<IDirect3DSurface9> depthstencil;
 			UINT width, height;
 			UINT drawcall_count, vertices_count;
@@ -83,14 +50,20 @@ namespace reshade::d3d9
 
 		bool init_backbuffer_texture();
 		bool init_default_depth_stencil();
-		bool init_fx_resources();
+		bool init_fullscreen_triangle_resources();
 
-		bool add_sampler(const reshadefx::sampler_info &info, d3d9_technique_data &technique_init);
 		bool init_texture(texture &info) override;
-		bool init_technique(technique &info, const d3d9_technique_data &technique_init, const std::unordered_map<std::string, com_ptr<IDirect3DVertexShader9>> &vs_entry_points, const std::unordered_map<std::string, com_ptr<IDirect3DPixelShader9>> &ps_entry_points);
+		void update_texture(texture &texture, const uint8_t *pixels) override;
+		bool update_texture_reference(texture &texture);
 
-		int get_best_preserve_starting_index();
-		bool check_depthstencil_size(D3DSURFACE_DESC desc);
+		bool compile_effect(effect_data &effect) override;
+
+		bool add_sampler(const reshadefx::sampler_info &info, struct d3d9_technique_data &technique_init);
+		bool init_technique(technique &info, struct d3d9_technique_data &&technique_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points);
+
+		void render_technique(const technique &technique) override;
+
+		bool check_depthstencil_size(const D3DSURFACE_DESC &desc);
 
 #if RESHADE_GUI
 		bool init_imgui_resources();
@@ -99,30 +72,30 @@ namespace reshade::d3d9
 #endif
 
 		void detect_depth_source();
-		bool create_depthstencil_replacement(IDirect3DSurface9 *depthstencil);
+		bool create_depthstencil_replacement(const com_ptr<IDirect3DSurface9> &depthstencil);
 
-		UINT _behavior_flags;
-		UINT _num_samplers;
-		UINT _num_simultaneous_rendertargets;
+		unsigned int _num_samplers;
+		unsigned int _num_simultaneous_rendertargets;
+		unsigned int _behavior_flags;
 		bool _disable_intz = false;
 		bool _preserve_depth_buffer = false;
 		bool _disable_depth_buffer_size_restriction = false;
-		int _preserve_starting_index = 0;
+		size_t _preserve_starting_index = 0;
 		bool _auto_preserve = true;
 		bool _auto_preserve_on_drawcalls = false;
 		bool _auto_preserve_on_vertices = true;
-		int _db_vertices = 0;
-		int _db_drawcalls = 0;
-		int _current_db_vertices = 0;
-		int _current_db_drawcalls = 0;
+		unsigned int _db_vertices = 0;
+		unsigned int _db_drawcalls = 0;
+		unsigned int _current_db_vertices = 0;
+		unsigned int _current_db_drawcalls = 0;
 		bool _is_multisampling_enabled = false;
 		D3DFORMAT _backbuffer_format = D3DFMT_UNKNOWN;
-		com_ptr<IDirect3DStateBlock9> _app_state;
+		state_block _app_state;
 		com_ptr<IDirect3DSurface9> _depthstencil;
 		com_ptr<IDirect3DSurface9> _depthstencil_replacement;
 		com_ptr<IDirect3DSurface9> _default_depthstencil;
-		std::unordered_map<IDirect3DSurface9 *, depth_source_info> _depth_source_table;
-		std::vector<depth_buffer_info> _depth_buffer_table;
+		std::unordered_map<com_ptr<IDirect3DSurface9>, depth_source_info> _depth_source_table;
+		std::vector<depth_source_info> _depth_buffer_table;
 
 		com_ptr<IDirect3DVertexBuffer9> _effect_triangle_buffer;
 		com_ptr<IDirect3DVertexDeclaration9> _effect_triangle_layout;

source/opengl/runtime_opengl.hpp
@@ -9,65 +9,10 @@
 #include "state_block.hpp"
 #include <unordered_set>
 
+namespace reshadefx { struct sampler_info; }
+
 namespace reshade::opengl
 {
-	struct opengl_tex_data : base_object
-	{
-		~opengl_tex_data()
-		{
-			if (should_delete)
-			{
-				glDeleteTextures(2, id);
-			}
-		}
-
-		bool should_delete = false;
-		GLuint id[2] = { };
-	};
-
-	struct opengl_sampler_data
-	{
-		GLuint id;
-		opengl_tex_data *texture;
-		bool is_srgb;
-		bool has_mipmaps;
-	};
-
-	struct opengl_pass_data : base_object
-	{
-		~opengl_pass_data()
-		{
-			if (program)
-				glDeleteProgram(program);
-			glDeleteFramebuffers(1, &fbo);
-		}
-
-		GLuint program = 0;
-		GLuint fbo = 0, draw_textures[8] = { };
-		GLint stencil_reference = 0;
-		GLuint stencil_mask = 0, stencil_read_mask = 0;
-		GLsizei viewport_width = 0, viewport_height = 0;
-		GLenum draw_buffers[8] = { };
-		GLenum stencil_func = GL_NONE, stencil_op_fail = GL_NONE, stencil_op_z_fail = GL_NONE, stencil_op_z_pass = GL_NONE;
-		GLenum blend_eq_color = GL_NONE, blend_eq_alpha = GL_NONE, blend_src = GL_NONE, blend_dest = GL_NONE, blend_src_alpha = GL_NONE, blend_dest_alpha = GL_NONE;
-		GLboolean color_mask[4] = { };
-		bool srgb = false, blend = false, stencil_test = false, clear_render_targets = true;
-	};
-
-	struct opengl_technique_data : base_object
-	{
-		~opengl_technique_data()
-		{
-			glDeleteQueries(1, &query);
-		}
-
-		GLuint query = 0;
-		bool query_in_flight = false;
-		std::vector<opengl_sampler_data> samplers;
-		ptrdiff_t uniform_storage_offset = 0;
-		ptrdiff_t uniform_storage_index = -1;
-	};
-
 	class runtime_opengl : public runtime
 	{
 	public:
@@ -76,17 +21,11 @@ namespace reshade::opengl
 		bool on_init(HWND hwnd, unsigned int width, unsigned int height);
 		void on_reset();
 		void on_present();
+
 		void on_draw_call(unsigned int vertices);
 		void on_fbo_attachment(GLenum target, GLenum attachment, GLenum objecttarget, GLuint object, GLint level);
 
-		void capture_frame(uint8_t *buffer) const override;
-		void update_texture(texture &texture, const uint8_t *data) override;
-		bool update_texture_reference(texture &texture);
-
-		bool compile_effect(effect_data &effect) override;
-		void unload_effects() override;
-
-		void render_technique(const technique &technique) override;
+		void capture_screenshot(uint8_t *buffer) const override;
 
 		std::unordered_set<HDC> _hdcs;
 
@@ -107,11 +46,18 @@ namespace reshade::opengl
 
 		bool init_backbuffer_texture();
 		bool init_default_depth_stencil();
-		bool init_fx_resources();
 
-		bool add_sampler(const reshadefx::sampler_info &info, opengl_technique_data &effect);
 		bool init_texture(texture &info) override;
-		bool init_technique(technique &info, const opengl_technique_data &effect, const std::unordered_map<std::string, GLuint> &entry_points, std::string &errors);
+		void update_texture(texture &texture, const uint8_t *data) override;
+		bool update_texture_reference(texture &texture);
+
+		bool compile_effect(effect_data &effect) override;
+		void unload_effects() override;
+
+		bool add_sampler(const reshadefx::sampler_info &info, struct opengl_technique_data &technique_init);
+		bool init_technique(technique &info, struct opengl_technique_data &&technique_init, const std::unordered_map<std::string, GLuint> &entry_points, std::string &errors);
+
+		void render_technique(const technique &technique) override;
 
 #if RESHADE_GUI
 		bool init_imgui_resources();
@@ -121,7 +67,7 @@ namespace reshade::opengl
 		void detect_depth_source();
 		void create_depth_texture(GLuint width, GLuint height, GLenum format);
 
-		state_block _stateblock;
+		state_block _app_state;
 		std::unordered_map<GLuint, depth_source_info> _depth_source_table;
 
 		GLuint _imgui_shader_program = 0, _imgui_VertHandle = 0, _imgui_FragHandle = 0;

commit 9daf3b021baee5f7ea074121200894eb1f1d690f

Fix technique initialization for multiple techniques
This was a regression introduced in commit 6c558f46d14412b31197abf3af6cfe2af857bddc

source/d3d10/runtime_d3d10.cpp
@@ -680,7 +680,7 @@ bool reshade::d3d10::runtime_d3d10::compile_effect(effect_data &effect)
 
 	for (technique &technique : _techniques)
 		if (technique.impl == nullptr && technique.effect_index == effect.index)
-			success &= init_technique(technique, std::move(technique_init), entry_points);
+			success &= init_technique(technique, technique_init, entry_points);
 
 	return success;
 }
@@ -761,9 +761,10 @@ bool reshade::d3d10::runtime_d3d10::add_sampler(const reshadefx::sampler_info &i
 
 	return true;
 }
-bool reshade::d3d10::runtime_d3d10::init_technique(technique &technique, d3d10_technique_data &&impl_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points)
+bool reshade::d3d10::runtime_d3d10::init_technique(technique &technique, const d3d10_technique_data &impl_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points)
 {
-	technique.impl = std::make_unique<d3d10_technique_data>(std::move(impl_init));
+	// Copy construct new technique implementation instead of move because effect may contain multiple techniques
+	technique.impl = std::make_unique<d3d10_technique_data>(impl_init);
 
 	const auto technique_data = technique.impl->as<d3d10_technique_data>();
 

source/d3d10/runtime_d3d10.hpp
@@ -48,7 +48,7 @@ namespace reshade::d3d10
 		void unload_effects() override;
 
 		bool add_sampler(const reshadefx::sampler_info &info, struct d3d10_technique_data &technique_init);
-		bool init_technique(technique &info, struct d3d10_technique_data &&technique_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points);
+		bool init_technique(technique &info, const struct d3d10_technique_data &technique_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points);
 
 		void render_technique(technique &technique) override;
 

source/d3d11/runtime_d3d11.cpp
@@ -693,7 +693,7 @@ bool reshade::d3d11::runtime_d3d11::compile_effect(effect_data &effect)
 
 	for (technique &technique : _techniques)
 		if (technique.impl == nullptr && technique.effect_index == effect.index)
-			success &= init_technique(technique, std::move(technique_init), entry_points);
+			success &= init_technique(technique, technique_init, entry_points);
 
 	return success;
 }
@@ -774,9 +774,10 @@ bool reshade::d3d11::runtime_d3d11::add_sampler(const reshadefx::sampler_info &i
 
 	return true;
 }
-bool reshade::d3d11::runtime_d3d11::init_technique(technique &technique, d3d11_technique_data &&impl_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points)
+bool reshade::d3d11::runtime_d3d11::init_technique(technique &technique, const d3d11_technique_data &impl_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points)
 {
-	technique.impl = std::make_unique<d3d11_technique_data>(std::move(impl_init));
+	// Copy construct new technique implementation instead of move because effect may contain multiple techniques
+	technique.impl = std::make_unique<d3d11_technique_data>(impl_init);
 
 	const auto technique_data = technique.impl->as<d3d11_technique_data>();
 

source/d3d11/runtime_d3d11.hpp
@@ -49,7 +49,7 @@ namespace reshade::d3d11
 		void unload_effects() override;
 
 		bool add_sampler(const reshadefx::sampler_info &info, struct d3d11_technique_data &technique_init);
-		bool init_technique(technique &info, struct d3d11_technique_data &&technique_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points);
+		bool init_technique(technique &info, const struct d3d11_technique_data &technique_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points);
 
 		void render_technique(technique &technique) override;
 

source/d3d9/runtime_d3d9.cpp
@@ -698,7 +698,7 @@ bool reshade::d3d9::runtime_d3d9::compile_effect(effect_data &effect)
 
 	for (technique &technique : _techniques)
 		if (technique.impl == nullptr && technique.effect_index == effect.index)
-			success &= init_technique(technique, std::move(technique_init), entry_points);
+			success &= init_technique(technique, technique_init, entry_points);
 
 	return success;
 }
@@ -735,9 +735,10 @@ bool reshade::d3d9::runtime_d3d9::add_sampler(const reshadefx::sampler_info &inf
 
 	return true;
 }
-bool reshade::d3d9::runtime_d3d9::init_technique(technique &technique, d3d9_technique_data &&impl_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points)
+bool reshade::d3d9::runtime_d3d9::init_technique(technique &technique, const d3d9_technique_data &impl_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points)
 {
-	technique.impl = std::make_unique<d3d9_technique_data>(std::move(impl_init));
+	// Copy construct new technique implementation instead of move because effect may contain multiple techniques
+	technique.impl = std::make_unique<d3d9_technique_data>(impl_init);
 
 	const auto technique_data = technique.impl->as<d3d9_technique_data>();
 

source/d3d9/runtime_d3d9.hpp
@@ -51,7 +51,7 @@ namespace reshade::d3d9
 		bool compile_effect(effect_data &effect) override;
 
 		bool add_sampler(const reshadefx::sampler_info &info, struct d3d9_technique_data &technique_init);
-		bool init_technique(technique &info, struct d3d9_technique_data &&technique_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points);
+		bool init_technique(technique &info, const struct d3d9_technique_data &technique_init, const std::unordered_map<std::string, com_ptr<IUnknown>> &entry_points);
 
 		void render_technique(technique &technique) override;
 

source/opengl/runtime_opengl.cpp
@@ -600,7 +600,7 @@ bool reshade::opengl::runtime_opengl::compile_effect(effect_data &effect)
 
 	for (technique &technique : _techniques)
 		if (technique.impl == nullptr && technique.effect_index == effect.index)
-			success &= init_technique(technique, std::move(technique_init), entry_points, effect.errors);
+			success &= init_technique(technique, technique_init, entry_points, effect.errors);
 
 	for (auto &it : entry_points)
 		glDeleteShader(it.second);
@@ -723,11 +723,12 @@ bool reshade::opengl::runtime_opengl::add_sampler(const reshadefx::sampler_info 
 
 	return true;
 }
-bool reshade::opengl::runtime_opengl::init_technique(technique &technique, opengl_technique_data &&impl_init, const std::unordered_map<std::string, GLuint> &entry_points, std::string &errors)
+bool reshade::opengl::runtime_opengl::init_technique(technique &technique, const opengl_technique_data &impl_init, const std::unordered_map<std::string, GLuint> &entry_points, std::string &errors)
 {
 	assert(_app_state.has_state);
 
-	technique.impl = std::make_unique<opengl_technique_data>(std::move(impl_init));
+	// Copy construct new technique implementation instead of move because effect may contain multiple techniques
+	technique.impl = std::make_unique<opengl_technique_data>(impl_init);
 
 	const auto technique_data = technique.impl->as<opengl_technique_data>();
 

source/opengl/runtime_opengl.hpp
@@ -48,7 +48,7 @@ namespace reshade::opengl
 		void unload_effects() override;
 
 		bool add_sampler(const reshadefx::sampler_info &info, struct opengl_technique_data &technique_init);
-		bool init_technique(technique &info, struct opengl_technique_data &&technique_init, const std::unordered_map<std::string, GLuint> &entry_points, std::string &errors);
+		bool init_technique(technique &info, const struct opengl_technique_data &technique_init, const std::unordered_map<std::string, GLuint> &entry_points, std::string &errors);
 
 		void render_technique(technique &technique) override;
 