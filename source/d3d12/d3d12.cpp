def6147ff7dea6677224ffd437fe00e9a2068ac6
crosire <crosiredev@gmail.com>
2019-05-05 16:07:11 +0200

    Add some missing D3D12 exports

--- source/d3d12/d3d12.cpp
+++ source/d3d12/d3d12.cpp
@@ -9,7 +9,11 @@
 
 extern reshade::log::message &operator<<(reshade::log::message &m, REFIID riid);
 
-HOOK_EXPORT HRESULT WINAPI D3D12CreateDevice(IUnknown *pAdapter, D3D_FEATURE_LEVEL MinimumFeatureLevel, REFIID riid, void **ppDevice)
+HOOK_EXPORT HRESULT WINAPI D3D12CreateDevice(
+	IUnknown *pAdapter,
+	D3D_FEATURE_LEVEL MinimumFeatureLevel,
+	REFIID riid,
+	void **ppDevice)
 {
 	LOG(INFO) << "Redirecting D3D12CreateDevice" << '(' << pAdapter << ", " << std::hex << MinimumFeatureLevel << std::dec << ", " << riid << ", " << ppDevice << ')' << " ...";
 
@@ -31,22 +35,58 @@ HOOK_EXPORT HRESULT WINAPI D3D12CreateDevice(IUnknown *pAdapter, D3D_FEATURE_LEV
 	*ppDevice = device_proxy;
 
 #if RESHADE_VERBOSE_LOG
-	LOG(DEBUG) << "Returning ID3D12Device" << device_proxy->_interface_version << " object " << device_proxy;
+	LOG(DEBUG) << "Returning ID3D12Device" << device_proxy->_interface_version << " object " << device_proxy << '.';
 #endif
 	return hr;
 }
 
-HOOK_EXPORT HRESULT WINAPI D3D12GetDebugInterface(REFIID riid, void **ppvDebug)
+HOOK_EXPORT HRESULT WINAPI D3D12GetDebugInterface(
+	REFIID riid,
+	void **ppvDebug)
 {
 	return reshade::hooks::call(D3D12GetDebugInterface)(riid, ppvDebug);
 }
 
-HOOK_EXPORT HRESULT WINAPI D3D12SerializeRootSignature(const D3D12_ROOT_SIGNATURE_DESC *pRootSignature, D3D_ROOT_SIGNATURE_VERSION Version, ID3DBlob **ppBlob, ID3DBlob **ppErrorBlob)
+HOOK_EXPORT HRESULT WINAPI D3D12CreateRootSignatureDeserializer(
+	LPCVOID pSrcData,
+	SIZE_T SrcDataSizeInBytes,
+	REFIID pRootSignatureDeserializerInterface,
+	void **ppRootSignatureDeserializer)
+{
+	return reshade::hooks::call(D3D12CreateRootSignatureDeserializer)(pSrcData, SrcDataSizeInBytes, pRootSignatureDeserializerInterface, ppRootSignatureDeserializer);
+}
+
+HOOK_EXPORT HRESULT WINAPI D3D12CreateVersionedRootSignatureDeserializer(
+	LPCVOID pSrcData,
+	SIZE_T SrcDataSizeInBytes,
+	REFIID pRootSignatureDeserializerInterface,
+	void **ppRootSignatureDeserializer)
+{
+	return reshade::hooks::call(D3D12CreateVersionedRootSignatureDeserializer)(pSrcData, SrcDataSizeInBytes, pRootSignatureDeserializerInterface, ppRootSignatureDeserializer);
+}
+
+HOOK_EXPORT HRESULT WINAPI D3D12EnableExperimentalFeatures(
+	UINT NumFeatures,
+	const IID *pIIDs,
+	void *pConfigurationStructs,
+	UINT *pConfigurationStructSizes)
+{
+	return reshade::hooks::call(D3D12EnableExperimentalFeatures)(NumFeatures, pIIDs, pConfigurationStructs, pConfigurationStructSizes);
+}
+
+HOOK_EXPORT HRESULT WINAPI D3D12SerializeRootSignature(
+	const D3D12_ROOT_SIGNATURE_DESC *pRootSignature,
+	D3D_ROOT_SIGNATURE_VERSION Version,
+	ID3DBlob **ppBlob,
+	ID3DBlob **ppErrorBlob)
 {
 	return reshade::hooks::call(D3D12SerializeRootSignature)(pRootSignature, Version, ppBlob, ppErrorBlob);
 }
 
-HOOK_EXPORT HRESULT WINAPI D3D12SerializeVersionedRootSignature(const D3D12_VERSIONED_ROOT_SIGNATURE_DESC *pRootSignature, ID3DBlob **ppBlob, ID3DBlob **ppErrorBlob)
+HOOK_EXPORT HRESULT WINAPI D3D12SerializeVersionedRootSignature(
+	const D3D12_VERSIONED_ROOT_SIGNATURE_DESC *pRootSignature,
+	ID3DBlob **ppBlob,
+	ID3DBlob **ppErrorBlob)
 {
 	return reshade::hooks::call(D3D12SerializeVersionedRootSignature)(pRootSignature, ppBlob, ppErrorBlob);
 }
