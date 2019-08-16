acfe8fba16ab1052c82b7af493fa7ad7f004b466
Cristian Buzduga <cristianbuzdug@yahoo.ro>
2019-08-16 14:45:32 +0300

    Revert "Added shortcut keys for switching backward or forward between presets. They can be configured in the settings page jut below "Effect Toggle Key". Also the switching is no longer circular, It does not go directly fromn the last to the first preset anymore. For example: when using depth of field filter with manual focus and  want to quickly go to the last preset (or the first), it will no longer go circular if pressing the shortcut key once more by accident."
    This reverts commit de71e7ede6fcedd24d2defc653682366f18f870a.

--- source/runtime.hpp
+++ source/runtime.hpp
@@ -272,8 +272,6 @@ namespace reshade
 		unsigned int _reload_key_data[4];
 		unsigned int _effects_key_data[4];
 		unsigned int _screenshot_key_data[4];
-		unsigned int _previous_preset_key_data[4];
-		unsigned int _next_preset_key_data[4];
 		int _screenshot_format = 1;
 		std::filesystem::path _screenshot_path;
 		std::filesystem::path _configuration_path;
@@ -328,9 +326,6 @@ namespace reshade
 		void draw_overlay_variable_editor();
 		void draw_overlay_technique_editor();
 		void draw_preset_explorer();
-
-		void get_preset_container(std::vector<std::filesystem::directory_entry> &preset_container);
-		bool switch_current_preset(bool bToNext);
 
 		std::vector<std::pair<std::string, std::function<void()>>> _menu_callables;
 		std::unique_ptr<texture> _imgui_font_atlas;
