ddf649c1a48cfc483f1744af553d5f315b14537f
crosire <crosiredev@gmail.com>
2019-10-15 20:28:05 +0200

    Change capitalization of some settings

--- source/gui.cpp
+++ source/gui.cpp
@@ -971,19 +971,19 @@ void reshade::runtime::draw_overlay_menu_settings()
 		modified |= imgui_key_input("Previous Preset Key", _previous_preset_key_data, *_input);
 		_ignore_shortcuts |= ImGui::IsItemActive();
 
-		modified |= ImGui::SliderInt("Preset transition\ndelay (ms)", &_preset_transition_delay, 0, 10 * 1000);
+		modified |= ImGui::SliderInt("Transition delay (ms)", &_preset_transition_delay, 0, 10 * 1000);
 		if (ImGui::IsItemHovered())
 			ImGui::SetTooltip("%s", "Makes a smooth transition, but only for floating point values.\nRecommended for multiple presets that contain the same shaders, otherwise set this to 0");
 
-		modified |= ImGui::Combo("Input Processing", &_input_processing_mode,
+		modified |= ImGui::Combo("Input processing", &_input_processing_mode,
 			"Pass on all input\0"
 			"Block input when cursor is on overlay\0"
 			"Block all input when overlay is visible\0");
 
-		modified |= imgui_path_list("Effect Search Paths", _effect_search_paths, _file_selection_path, g_reshade_dll_path.parent_path());
-		modified |= imgui_path_list("Texture Search Paths", _texture_search_paths, _file_selection_path, g_reshade_dll_path.parent_path());
+		modified |= imgui_path_list("Effect search paths", _effect_search_paths, _file_selection_path, g_reshade_dll_path.parent_path());
+		modified |= imgui_path_list("Texture search paths", _texture_search_paths, _file_selection_path, g_reshade_dll_path.parent_path());
 
-		if (ImGui::Button("Restart Tutorial", ImVec2(ImGui::CalcItemWidth(), 0)))
+		if (ImGui::Button("Restart tutorial", ImVec2(ImGui::CalcItemWidth(), 0)))
 			_tutorial_index = 0;
 	}
 
