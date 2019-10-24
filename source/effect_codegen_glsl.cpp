crosire/reshade

commit e986a854b1928856945197a3d7e3558c61558b0c

Reduce code size of generated HLSL and GLSL to speed up compile times

source/effect_codegen_glsl.cpp
@@ -30,6 +30,8 @@ private:
 		general,
 		// This is a special name that is not modified and should be unique
 		reserved,
+		// Replace name with a code snippet
+		expression,
 	};
 
 	std::string _ubo_block;
@@ -232,8 +234,9 @@ private:
 	void define_name(const id id, std::string name)
 	{
 		assert(!name.empty());
-		if (name[0] == '_')
-			return; // Filter out names that may clash with automatic ones
+		if constexpr (naming != naming::expression)
+			if (name[0] == '_')
+				return; // Filter out names that may clash with automatic ones
 		if constexpr (naming != naming::reserved)
 			name = escape_name(name);
 		if constexpr (naming == naming::general)
@@ -694,20 +697,7 @@ private:
 
 		const id res = make_id();
 
-		std::string &code = _blocks.at(_current_block);
-
-		write_location(code, exp.location);
-
-		code += '\t';
-		write_type(code, exp.type);
-		code += ' ' + id_to_name(res);
-
-		if (exp.type.is_array())
-			code += '[' + std::to_string(exp.type.array_length) + ']';
-
-		code += " = ";
-
-		std::string newcode = id_to_name(exp.base);
+		std::string code = id_to_name(exp.base);
 
 		for (const auto &op : exp.chain)
 		{
@@ -715,17 +705,17 @@ private:
 			{
 			case expression::operation::op_cast:
 				{ std::string type; write_type<false, false>(type, op.to);
-				newcode = type + '(' + newcode + ')'; }
+				code = type + '(' + code + ')'; }
 				break;
 			case expression::operation::op_member:
-				newcode += '.';
-				newcode += escape_name(find_struct(op.from.definition).member_list[op.index].name);
+				code += '.';
+				code += escape_name(find_struct(op.from.definition).member_list[op.index].name);
 				break;
 			case expression::operation::op_dynamic_index:
-				newcode += '[' + id_to_name(op.index) + ']';
+				code += '[' + id_to_name(op.index) + ']';
 				break;
 			case expression::operation::op_constant_index:
-				newcode += '[' + std::to_string(op.index) + ']';
+				code += '[' + std::to_string(op.index) + ']';
 				break;
 			case expression::operation::op_swizzle:
 				if (op.from.is_matrix())
@@ -735,7 +725,7 @@ private:
 						const unsigned int row = op.swizzle[0] % 4;
 						const unsigned int col = (op.swizzle[0] - row) / 4;
 
-						newcode += '[' + std::to_string(row) + "][" + std::to_string(col) + ']';
+						code += '[' + std::to_string(row) + "][" + std::to_string(col) + ']';
 					}
 					else
 					{
@@ -745,16 +735,16 @@ private:
 				}
 				else
 				{
-					newcode += '.';
+					code += '.';
 					for (unsigned int i = 0; i < 4 && op.swizzle[i] >= 0; ++i)
-						newcode += "xyzw"[op.swizzle[i]];
+						code += "xyzw"[op.swizzle[i]];
 				}
 				break;
 			}
 		}
 
-		code += newcode;
-		code += ";\n";
+		// Avoid excessive variable definitions by instancing simple load operations in code every time
+		define_name<naming::expression>(res, std::move(code));
 
 		return res;
 	}
@@ -820,27 +810,32 @@ private:
 	{
 		const id res = make_id();
 
-		std::string &code = _blocks.at(_current_block);
-
-		// Struct initialization is not supported right now
-		if (type.is_struct())
+		if (type.is_array() || type.is_struct())
 		{
-			code += '\t';
+			std::string &code = _blocks.at(_current_block);
+
+			// Array constants need to be stored in a constant variable as they cannot be used in-place
+			code += "\tconst ";
 			write_type(code, type);
-			code += ' ' + id_to_name(res) + ";\n";
+			code += ' ' + id_to_name(res);
+
+			if (type.is_array())
+				code += '[' + std::to_string(type.array_length) + ']';
+
+			// Struct initialization is not supported right now
+			if (!type.is_struct())
+			{
+				code += " = ";
+				write_constant(code, type, data);
+			}
+
+			code += ";\n";
 			return res;
 		}
 
-		code += "\tconst ";
-		write_type(code, type);
-		code += ' ' + id_to_name(res);
-
-		if (type.is_array())
-			code += '[' + std::to_string(type.array_length) + ']';
-
-		code += " = ";
+		std::string code;
 		write_constant(code, type, data);
-		code += ";\n";
+		define_name<naming::expression>(res, std::move(code));
 
 		return res;
 	}

source/effect_codegen_hlsl.cpp
@@ -27,6 +27,8 @@ private:
 		unique,
 		// Will be numbered when clashing with another name
 		general,
+		// Replace name with a code snippet
+		expression,
 	};
 
 	std::string _cbuffer_block;
@@ -219,6 +221,26 @@ private:
 		s += '\n';
 	}
 
+	std::string id_to_name(id id) const
+	{
+		if (const auto it = _names.find(id); it != _names.end())
+			return it->second;
+		return '_' + std::to_string(id);
+	}
+
+	template <naming naming = naming::general>
+	void define_name(const id id, std::string name)
+	{
+		assert(!name.empty());
+		if constexpr (naming != naming::expression)
+			if (name[0] == '_')
+				return; // Filter out names that may clash with automatic ones
+		if constexpr (naming == naming::general)
+			if (std::find_if(_names.begin(), _names.end(), [&name](const auto &it) { return it.second == name; }) != _names.end())
+				name += '_' + std::to_string(id);
+		_names[id] = std::move(name);
+	}
+
 	std::string convert_semantic(const std::string &semantic) const
 	{
 		if (_shader_model < 40)
@@ -245,25 +267,6 @@ private:
 		}
 
 		return semantic;
-	}
-
-	std::string id_to_name(id id) const
-	{
-		if (const auto it = _names.find(id); it != _names.end())
-			return it->second;
-		return '_' + std::to_string(id);
-	}
-
-	template <naming naming = naming::general>
-	void define_name(const id id, std::string name)
-	{
-		assert(!name.empty());
-		if (name[0] == '_')
-			return; // Filter out names that may clash with automatic ones
-		if constexpr (naming == naming::general)
-			if (std::find_if(_names.begin(), _names.end(), [&name](const auto &it) { return it.second == name; }) != _names.end())
-				name += '_' + std::to_string(id);
-		_names[id] = std::move(name);
 	}
 
 	static void increase_indentation_level(std::string &block)
@@ -482,7 +485,7 @@ private:
 			name += '_';
 
 		info.definition = make_id();
-		define_name<naming::unique>(info.definition, name);
+		define_name<naming::unique>(info.definition, std::move(name));
 
 		std::string &code = _blocks.at(_current_block);
 
@@ -630,19 +633,6 @@ private:
 
 		const id res = make_id();
 
-		std::string &code = _blocks.at(_current_block);
-
-		write_location(code, exp.location);
-
-		code += '\t';
-		write_type(code, exp.type);
-		code += ' ' + id_to_name(res);
-
-		if (exp.type.is_array())
-			code += '[' + std::to_string(exp.type.array_length) + ']';
-
-		code += " = ";
-
 		static const char s_matrix_swizzles[16][5] = {
 			"_m00", "_m01", "_m02", "_m03",
 			"_m10", "_m11", "_m12", "_m13",
@@ -650,7 +640,7 @@ private:
 			"_m30", "_m31", "_m32", "_m33"
 		};
 
-		std::string newcode = id_to_name(exp.base);
+		std::string code = id_to_name(exp.base);
 
 		for (const auto &op : exp.chain)
 		{
@@ -658,31 +648,31 @@ private:
 			{
 			case expression::operation::op_cast:
 				{ std::string type; write_type<false, false>(type, op.to);
-				newcode = "((" + type + ')' + newcode + ')'; } // Cast in parentheses so that a subsequent operation operates on the casted value
+				code = "((" + type + ')' + code + ')'; } // Cast in parentheses so that a subsequent operation operates on the casted value
 				break;
 			case expression::operation::op_member:
-				newcode += '.';
-				newcode += find_struct(op.from.definition).member_list[op.index].name;
+				code += '.';
+				code += find_struct(op.from.definition).member_list[op.index].name;
 				break;
 			case expression::operation::op_constant_index:
-				newcode += '[' + std::to_string(op.index) + ']';
+				code += '[' + std::to_string(op.index) + ']';
 				break;
 			case expression::operation::op_dynamic_index:
-				newcode += '[' + id_to_name(op.index) + ']';
+				code += '[' + id_to_name(op.index) + ']';
 				break;
 			case expression::operation::op_swizzle:
-				newcode += '.';
+				code += '.';
 				for (unsigned int i = 0; i < 4 && op.swizzle[i] >= 0; ++i)
 					if (op.from.is_matrix())
-						newcode += s_matrix_swizzles[op.swizzle[i]];
+						code += s_matrix_swizzles[op.swizzle[i]];
 					else
-						newcode += "xyzw"[op.swizzle[i]];
+						code += "xyzw"[op.swizzle[i]];
 				break;
 			}
 		}
 
-		code += newcode;
-		code += ";\n";
+		// Avoid excessive variable definitions by instancing simple load operations in code every time
+		define_name<naming::expression>(res, std::move(code));
 
 		return res;
 	}
@@ -733,18 +723,24 @@ private:
 	{
 		const id res = make_id();
 
-		std::string &code = _blocks.at(_current_block);
-
-		code += "\tconst ";
-		write_type(code, type);
-		code += ' ' + id_to_name(res);
-
 		if (type.is_array())
-			code += '[' + std::to_string(type.array_length) + ']';
+		{
+			std::string &code = _blocks.at(_current_block);
 
-		code += " = ";
+			// Array constants need to be stored in a constant variable as they cannot be used in-place
+			code += "\tconst ";
+			write_type(code, type);
+			code += ' ' + id_to_name(res);
+			code += '[' + std::to_string(type.array_length) + ']';
+			code += " = ";
+			write_constant(code, type, data);
+			code += ";\n";
+			return res;
+		}
+
+		std::string code;
 		write_constant(code, type, data);
-		code += ";\n";
+		define_name<naming::expression>(res, std::move(code));
 
 		return res;
 	}

commit 35070fc7f06aec36e0744bc9e694799dd711e031

Fix postfix operators being translated to wrong HLSL/GLSL code again
This was broken by e986a854b1928856945197a3d7e3558c61558b0c

source/effect_codegen_glsl.cpp
@@ -697,7 +697,7 @@ private:
 
 		const id res = make_id();
 
-		std::string code = id_to_name(exp.base);
+		std::string expr_code = id_to_name(exp.base);
 
 		for (const auto &op : exp.chain)
 		{
@@ -705,17 +705,17 @@ private:
 			{
 			case expression::operation::op_cast:
 				{ std::string type; write_type<false, false>(type, op.to);
-				code = type + '(' + code + ')'; }
+				expr_code = type + '(' + expr_code + ')'; }
 				break;
 			case expression::operation::op_member:
-				code += '.';
-				code += escape_name(find_struct(op.from.definition).member_list[op.index].name);
+				expr_code += '.';
+				expr_code += escape_name(find_struct(op.from.definition).member_list[op.index].name);
 				break;
 			case expression::operation::op_dynamic_index:
-				code += '[' + id_to_name(op.index) + ']';
+				expr_code += '[' + id_to_name(op.index) + ']';
 				break;
 			case expression::operation::op_constant_index:
-				code += '[' + std::to_string(op.index) + ']';
+				expr_code += '[' + std::to_string(op.index) + ']';
 				break;
 			case expression::operation::op_swizzle:
 				if (op.from.is_matrix())
@@ -725,7 +725,7 @@ private:
 						const unsigned int row = op.swizzle[0] % 4;
 						const unsigned int col = (op.swizzle[0] - row) / 4;
 
-						code += '[' + std::to_string(row) + "][" + std::to_string(col) + ']';
+						expr_code += '[' + std::to_string(row) + "][" + std::to_string(col) + ']';
 					}
 					else
 					{
@@ -735,16 +735,27 @@ private:
 				}
 				else
 				{
-					code += '.';
+					expr_code += '.';
 					for (unsigned int i = 0; i < 4 && op.swizzle[i] >= 0; ++i)
-						code += "xyzw"[op.swizzle[i]];
+						expr_code += "xyzw"[op.swizzle[i]];
 				}
 				break;
 			}
 		}
 
-		// Avoid excessive variable definitions by instancing simple load operations in code every time
-		define_name<naming::expression>(res, std::move(code));
+		if (force_new_id)
+		{
+			// Need to store value in a new variable to comply with request for a new ID
+			std::string &code = _blocks.at(_current_block);
+
+			write_type(code, exp.type);
+			code += ' ' + id_to_name(res) + " = " + expr_code + ";\n";
+		}
+		else
+		{
+			// Avoid excessive variable definitions by instancing simple load operations in code every time
+			define_name<naming::expression>(res, std::move(expr_code));
+		}
 
 		return res;
 	}

source/effect_codegen_hlsl.cpp
@@ -640,7 +640,7 @@ private:
 			"_m30", "_m31", "_m32", "_m33"
 		};
 
-		std::string code = id_to_name(exp.base);
+		std::string expr_code = id_to_name(exp.base);
 
 		for (const auto &op : exp.chain)
 		{
@@ -648,31 +648,42 @@ private:
 			{
 			case expression::operation::op_cast:
 				{ std::string type; write_type<false, false>(type, op.to);
-				code = "((" + type + ')' + code + ')'; } // Cast in parentheses so that a subsequent operation operates on the casted value
+				expr_code = "((" + type + ')' + expr_code + ')'; } // Cast in parentheses so that a subsequent operation operates on the casted value
 				break;
 			case expression::operation::op_member:
-				code += '.';
-				code += find_struct(op.from.definition).member_list[op.index].name;
-				break;
-			case expression::operation::op_constant_index:
-				code += '[' + std::to_string(op.index) + ']';
+				expr_code += '.';
+				expr_code += find_struct(op.from.definition).member_list[op.index].name;
 				break;
 			case expression::operation::op_dynamic_index:
-				code += '[' + id_to_name(op.index) + ']';
+				expr_code += '[' + id_to_name(op.index) + ']';
+				break;
+			case expression::operation::op_constant_index:
+				expr_code += '[' + std::to_string(op.index) + ']';
 				break;
 			case expression::operation::op_swizzle:
-				code += '.';
+				expr_code += '.';
 				for (unsigned int i = 0; i < 4 && op.swizzle[i] >= 0; ++i)
 					if (op.from.is_matrix())
-						code += s_matrix_swizzles[op.swizzle[i]];
+						expr_code += s_matrix_swizzles[op.swizzle[i]];
 					else
-						code += "xyzw"[op.swizzle[i]];
+						expr_code += "xyzw"[op.swizzle[i]];
 				break;
 			}
 		}
 
-		// Avoid excessive variable definitions by instancing simple load operations in code every time
-		define_name<naming::expression>(res, std::move(code));
+		if (force_new_id)
+		{
+			// Need to store value in a new variable to comply with request for a new ID
+			std::string &code = _blocks.at(_current_block);
+
+			write_type(code, exp.type);
+			code += ' ' + id_to_name(res) + " = " + expr_code + ";\n";
+		}
+		else
+		{
+			// Avoid excessive variable definitions by instancing simple load operations in code every time
+			define_name<naming::expression>(res, std::move(expr_code));
+		}
 
 		return res;
 	}