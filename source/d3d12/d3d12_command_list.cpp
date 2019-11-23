4951efdcaf607726d6c90636b2ae29da8166edbd
crosire <crosiredev@gmail.com>
2019-11-23 21:57:50 +0100

    Remove logging for "ID3D12GraphicsCommandList" destroy events

--- source/d3d12/d3d12_command_list.cpp
+++ source/d3d12/d3d12_command_list.cpp
@@ -83,9 +83,6 @@ ULONG   STDMETHODCALLTYPE D3D12GraphicsCommandList::Release()
 	if (ref_orig != 0)
 		LOG(WARN) << "Reference count for ID3D12GraphicsCommandList" << _interface_version << " object " << this << " is inconsistent.";
 
-#if RESHADE_VERBOSE_LOG
-	LOG(DEBUG) << "Destroyed ID3D12GraphicsCommandList" << _interface_version << " object " << this << '.';
-#endif
 	delete this;
 
 	return 0;
