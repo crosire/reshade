5e72f049cb5f3e25fa12c9b05f22395540cfd2fc
crosire <crosiredev@gmail.com>
2020-01-23 20:34:32 +0100

    Remove "VK_IMAGE_USAGE_SAMPLED_BIT" from effect depth stencil resource

--- source/vulkan/runtime_vk.cpp
+++ source/vulkan/runtime_vk.cpp
@@ -194,7 +194,7 @@ bool reshade::vulkan::runtime_vk::on_init(VkSwapchainKHR swapchain, const VkSwap
 	// Create effect depth stencil resource
 	_effect_depthstencil = create_image(
 		_width, _height, 1, VK_FORMAT_D24_UNORM_S8_UINT,
-		VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+		VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
 		VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
 	if (_effect_depthstencil == VK_NULL_HANDLE)
 		return false;
