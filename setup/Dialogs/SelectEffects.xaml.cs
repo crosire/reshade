1f8df3187f51d565dbf3653448a46495d475fb02
crosire <crosiredev@gmail.com>
2020-02-29 21:35:34 +0100

    Add more effect packages to setup tool

--- setup/Dialogs/SelectEffects.xaml.cs
+++ setup/Dialogs/SelectEffects.xaml.cs
@@ -14,7 +14,7 @@ namespace ReShade.Setup
 {
 	public class EffectPackage : INotifyPropertyChanged
 	{
-		public bool Enabled { get; set; } = true;
+		public bool Enabled { get; set; }
 		public string PackageName { get; set; }
 		public string PackageDescription { get; set; }
 		public string DownloadUrl { get; set; }
@@ -33,7 +33,62 @@ namespace ReShade.Setup
 			InitializeComponent();
 			DataContext = this;
 
-			Packages.Add(new EffectPackage { PackageName = "Standard effects", PackageDescription = "https://github.com/crosire/reshade-shaders", DownloadUrl = "https://github.com/crosire/reshade-shaders/archive/master.zip" });
+			Packages.Add(new EffectPackage {
+				Enabled = true,
+				PackageName = "Standard effects",
+				PackageDescription = "https://github.com/crosire/reshade-shaders",
+				DownloadUrl = "https://github.com/crosire/reshade-shaders/archive/master.zip"
+			});
+			Packages.Add(new EffectPackage {
+				PackageName = "Color effects by prod80",
+				PackageDescription = "https://github.com/prod80/prod80-ReShade-Repository",
+				DownloadUrl = "https://github.com/prod80/prod80-ReShade-Repository/archive/master.zip"
+			});
+			Packages.Add(new EffectPackage {
+				PackageName = "qUINT by Marty McFly",
+				PackageDescription = "https://github.com/martymcmodding/qUINT",
+				DownloadUrl = "https://github.com/martymcmodding/qUINT/archive/master.zip"
+			});
+			Packages.Add(new EffectPackage {
+				PackageName = "Depth3D by BlueSkyDefender",
+				PackageDescription = "https://github.com/BlueSkyDefender/Depth3D",
+				DownloadUrl = "https://github.com/BlueSkyDefender/Depth3D/archive/master.zip"
+			});
+			Packages.Add(new EffectPackage {
+				PackageName = "OtisFX by Otis",
+				PackageDescription = "https://github.com/FransBouma/OtisFX",
+				DownloadUrl = "https://github.com/FransBouma/OtisFX/archive/master.zip"
+			});
+			Packages.Add(new EffectPackage {
+				PackageName = "luluco250's effects",
+				PackageDescription = "https://github.com/luluco250/FXShaders",
+				DownloadUrl = "https://github.com/luluco250/FXShaders/archive/master.zip"
+			});
+			Packages.Add(new EffectPackage {
+				PackageName = "Fubax ReShade FX shaders",
+				PackageDescription = "https://github.com/Fubaxiusz/fubax-shaders",
+				DownloadUrl = "https://github.com/Fubaxiusz/fubax-shaders/archive/master.zip"
+			});
+			Packages.Add(new EffectPackage {
+				PackageName = "Daodan's effects",
+				PackageDescription = "https://github.com/Daodan317081/reshade-shaders",
+				DownloadUrl = "https://github.com/Daodan317081/reshade-shaders/archive/master.zip"
+			});
+			Packages.Add(new EffectPackage {
+				PackageName = "brussell's effects",
+				PackageDescription = "https://github.com/brussell1/Shaders",
+				DownloadUrl = "https://github.com/brussell1/Shaders/archive/master.zip"
+			});
+			Packages.Add(new EffectPackage {
+				PackageName = "Pirate Shaders by Heathen",
+				PackageDescription = "https://github.com/Heathen/Pirate-Shaders",
+				DownloadUrl = "https://github.com/Heathen/Pirate-Shaders/archive/master.zip"
+			});
+			Packages.Add(new EffectPackage {
+				PackageName = "PPFX by Euda",
+				PackageDescription = "https://github.com/pascalmatthaeus/ppfx",
+				DownloadUrl = "https://github.com/pascalmatthaeus/ppfx/archive/master.zip"
+			});
 		}
 
 		public string[] EnabledPackageUrls => Packages.Where(x => x.Enabled).Select(x => x.DownloadUrl).ToArray();
