faf50442ae0be042fd1a327025b8bf339f136a03
crosire <crosiredev@gmail.com>
2020-11-26 21:47:01 +0100

    Do not count network traffic for local sockets

--- source/windows/ws2_32.cpp
+++ source/windows/ws2_32.cpp
@@ -6,21 +6,51 @@
 #include "hook_manager.hpp"
 #include <Windows.h>
 #include <Winsock2.h>
+#include <ws2ipdef.h>
+
+// Do not count network traffic for local sockets (localhost), so to avoid blocking occuring in games running local servers in single player too
+static bool is_local_socket(SOCKET s)
+{
+	// Get the target address information of the socket
+	int address_size = sizeof(SOCKADDR_STORAGE);
+	SOCKADDR_STORAGE peer_address = {};
+	if (getpeername(s, reinterpret_cast<PSOCKADDR>(&peer_address), &address_size) == SOCKET_ERROR)
+		return false;
+
+	const int32_t ipv4_loopback = INADDR_LOOPBACK; // 127.0.0.1
+	const uint8_t ipv6_loopback[16] = IN6ADDR_LOOPBACK_INIT; // ::1
+	const uint8_t ipv6_ipv4_loopback[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 127, 0, 0, 1 }; // ::ffff:127.0.0.1
+	static_assert(sizeof(ipv4_loopback) == sizeof(IN_ADDR) && sizeof(ipv6_loopback) == sizeof(IN6_ADDR));
+
+	// Check if the target address matches the loopback address, in which case the connection is local
+	switch (peer_address.ss_family)
+	{
+	default:
+		return false;
+	case AF_INET:
+		return std::memcmp(&reinterpret_cast<PSOCKADDR_IN >(&peer_address)->sin_addr, &ipv4_loopback, sizeof(IN_ADDR)) == 0;
+	case AF_INET6:
+		return std::memcmp(&reinterpret_cast<PSOCKADDR_IN6>(&peer_address)->sin6_addr, ipv6_loopback, sizeof(IN6_ADDR)) == 0 ||
+		       std::memcmp(&reinterpret_cast<PSOCKADDR_IN6>(&peer_address)->sin6_addr, ipv6_ipv4_loopback, sizeof(IN6_ADDR)) == 0;
+	}
+}
 
 volatile long g_network_traffic = 0;
 
 HOOK_EXPORT int WSAAPI HookWSASend(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesSent, DWORD dwFlags, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine)
 {
-	for (DWORD i = 0; i < dwBufferCount; ++i)
-		InterlockedAdd(&g_network_traffic, lpBuffers[i].len);
+	if (!is_local_socket(s))
+		for (DWORD i = 0; i < dwBufferCount; ++i)
+			InterlockedAdd(&g_network_traffic, lpBuffers[i].len);
 
 	static const auto trampoline = reshade::hooks::call(HookWSASend);
 	return trampoline(s, lpBuffers, dwBufferCount, lpNumberOfBytesSent, dwFlags, lpOverlapped, lpCompletionRoutine);
 }
 HOOK_EXPORT int WSAAPI HookWSASendTo(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesSent, DWORD dwFlags, const struct sockaddr *lpTo, int iToLen, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine)
 {
-	for (DWORD i = 0; i < dwBufferCount; ++i)
-		InterlockedAdd(&g_network_traffic, lpBuffers[i].len);
+	if (!is_local_socket(s))
+		for (DWORD i = 0; i < dwBufferCount; ++i)
+			InterlockedAdd(&g_network_traffic, lpBuffers[i].len);
 
 	static const auto trampoline = reshade::hooks::call(HookWSASendTo);
 	return trampoline(s, lpBuffers, dwBufferCount, lpNumberOfBytesSent, dwFlags, lpTo, iToLen, lpOverlapped, lpCompletionRoutine);
@@ -30,7 +60,7 @@ HOOK_EXPORT int WSAAPI HookWSARecv(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferC
 	static const auto trampoline = reshade::hooks::call(HookWSARecv);
 	const auto status = trampoline(s, lpBuffers, dwBufferCount, lpNumberOfBytesRecvd, lpFlags, lpOverlapped, lpCompletionRoutine);
 
-	if (status == 0 && lpNumberOfBytesRecvd != nullptr)
+	if (status == 0 && lpNumberOfBytesRecvd != nullptr && !is_local_socket(s))
 		InterlockedAdd(&g_network_traffic, *lpNumberOfBytesRecvd);
 
 	return status;
@@ -40,7 +70,7 @@ HOOK_EXPORT int WSAAPI HookWSARecvFrom(SOCKET s, LPWSABUF lpBuffers, DWORD dwBuf
 	static const auto trampoline = reshade::hooks::call(HookWSARecvFrom);
 	const auto status = trampoline(s, lpBuffers, dwBufferCount, lpNumberOfBytesRecvd, lpFlags, lpFrom, lpFromlen, lpOverlapped, lpCompletionRoutine);
 
-	if (status == 0 && lpNumberOfBytesRecvd != nullptr)
+	if (status == 0 && lpNumberOfBytesRecvd != nullptr && !is_local_socket(s))
 		InterlockedAdd(&g_network_traffic, *lpNumberOfBytesRecvd);
 
 	return status;
@@ -51,7 +81,7 @@ HOOK_EXPORT int WSAAPI HookSend(SOCKET s, const char *buf, int len, int flags)
 	static const auto trampoline = reshade::hooks::call(HookSend);
 	const auto num_bytes_send = trampoline(s, buf, len, flags);
 
-	if (num_bytes_send != SOCKET_ERROR)
+	if (num_bytes_send != SOCKET_ERROR && !is_local_socket(s))
 		InterlockedAdd(&g_network_traffic, num_bytes_send);
 
 	return num_bytes_send;
@@ -61,7 +91,7 @@ HOOK_EXPORT int WSAAPI HookSendTo(SOCKET s, const char *buf, int len, int flags,
 	static const auto trampoline = reshade::hooks::call(HookSendTo);
 	const auto num_bytes_send = trampoline(s, buf, len, flags, to, tolen);
 
-	if (num_bytes_send != SOCKET_ERROR)
+	if (num_bytes_send != SOCKET_ERROR && !is_local_socket(s))
 		InterlockedAdd(&g_network_traffic, num_bytes_send);
 
 	return num_bytes_send;
@@ -71,7 +101,7 @@ HOOK_EXPORT int WSAAPI HookRecv(SOCKET s, char *buf, int len, int flags)
 	static const auto trampoline = reshade::hooks::call(HookRecv);
 	const auto num_bytes_recieved = trampoline(s, buf, len, flags);
 
-	if (num_bytes_recieved != SOCKET_ERROR)
+	if (num_bytes_recieved != SOCKET_ERROR && !is_local_socket(s))
 		InterlockedAdd(&g_network_traffic, num_bytes_recieved);
 
 	return num_bytes_recieved;
@@ -81,7 +111,7 @@ HOOK_EXPORT int WSAAPI HookRecvFrom(SOCKET s, char *buf, int len, int flags, str
 	static const auto trampoline = reshade::hooks::call(HookRecvFrom);
 	const auto num_bytes_recieved = trampoline(s, buf, len, flags, from, fromlen);
 
-	if (num_bytes_recieved != SOCKET_ERROR)
+	if (num_bytes_recieved != SOCKET_ERROR && !is_local_socket(s))
 		InterlockedAdd(&g_network_traffic, num_bytes_recieved);
 
 	return num_bytes_recieved;
