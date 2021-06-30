756f44e6d5625f9f9b49b9fa0bb9eb394ec5f578
kingeric1992 <king_eric1992@hotmail.com>
2021-06-30 18:01:05 +0800

    change file encoding from ISO-8859 (28591) to utf-8 with BOM

--- source/input.cpp
+++ source/input.cpp
@@ -1,4 +1,4 @@
-/*
+ï»¿/*
  * Copyright (C) 2014 Patrick Mours. All rights reserved.
  * License: https://github.com/crosire/reshade#license
  */
@@ -421,9 +421,9 @@ std::string reshade::input::key_name(unsigned int keycode)
 		"F17", "F18", "F19", "F20", "F21", "F22", "F23", "F24", "", "", "", "", "", "", "", "",
 		"Num Lock", "Scroll Lock", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
 		"Left Shift", "Right Shift", "Left Control", "Right Control", "Left Menu", "Right Menu", "Browser Back", "Browser Forward", "Browser Refresh", "Browser Stop", "Browser Search", "Browser Favorites", "Browser Home", "Volume Mute", "Volume Down", "Volume Up",
-		"Next Track", "Previous Track", "Media Stop", "Media Play/Pause", "Mail", "Media Select", "Launch App 1", "Launch App 2", "", "", u8"Ü", "OEM +", "OEM ,", "OEM -", "OEM .", "OEM #",
-		u8"Ö", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-		"", "", "", "", "", "", "", "", "", "", "", u8"OEM ß", "OEM ^", u8"OEM ´", u8"Ä", "OEM 8",
+		"Next Track", "Previous Track", "Media Stop", "Media Play/Pause", "Mail", "Media Select", "Launch App 1", "Launch App 2", "", "", u8"Ãœ", "OEM +", "OEM ,", "OEM -", "OEM .", "OEM #",
+		u8"Ã–", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+		"", "", "", "", "", "", "", "", "", "", "", u8"OEM ÃŸ", "OEM ^", u8"OEM Â´", u8"Ã„", "OEM 8",
 		"", "", "OEM <", "", "", "", "", "", "", "", "", "", "", "", "", "",
 		"", "", "", "", "", "", "Attn", "CrSel", "ExSel", "Erase EOF", "Play", "Zoom", "", "PA1", "OEM Clear", ""
 	};
