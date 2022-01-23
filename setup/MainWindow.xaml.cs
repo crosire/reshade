223c123d9f36bb89a7d4a97fd81f995935fcef97
crosire <crosiredev@gmail.com>
2022-01-23 16:25:34 +0100

    Fix setup tool writing to wrong configuration file when updating

--- setup/MainWindow.xaml.cs
+++ setup/MainWindow.xaml.cs
@@ -580,7 +580,7 @@ namespace ReShade.Setup
 				modulePath = Path.Combine(basePath, modulePath);
 
 				var configPathAlt = Path.ChangeExtension(modulePath, ".ini");
-				if (File.Exists(configPathAlt))
+				if (File.Exists(configPathAlt) && !File.Exists(configPath))
 				{
 					configPath = configPathAlt;
 				}
