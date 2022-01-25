f65ba7e47d74a2aab25302bd0c71560c451fff59
crosire <crosiredev@gmail.com>
2022-01-25 22:49:21 +0100

    Fix effects using "line" as variable name failing to compile for shader model 3

--- source/effect_codegen_hlsl.cpp
+++ source/effect_codegen_hlsl.cpp
@@ -351,7 +351,8 @@ private:
 		};
 
 		// HLSL compiler complains about "technique" and "pass" names in strict mode (no matter the casing)
-		if (stringicmp(name, "pass") ||
+		if (stringicmp(name, "line") ||
+			stringicmp(name, "pass") ||
 			stringicmp(name, "technique"))
 			// This is guaranteed to not clash with user defined names, since those starting with an underscore are filtered out in 'define_name'
 			name = '_' + name;
