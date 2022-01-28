64c5bf6d0bf8e3661334e4973354b2b452d699e4
crosire <crosiredev@gmail.com>
2022-01-28 01:05:06 +0100

    Fix resource compiler not handling UTF-8 encoding

--- res/resource.h
+++ res/resource.h
@@ -29,6 +29,6 @@
 #define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           102
+#define _APS_NEXT_SYMED_VALUE           111
 #endif
 #endif
