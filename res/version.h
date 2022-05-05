3d142cbba6b7eb0b54ae2b0a4ee52352f57cffeb
Zayveeo5e <37855671+Zayveeo5e@users.noreply.github.com>
2022-05-05 07:35:01 -0500

    Update .git ignore and add version.h

---
+++ res/version.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define VERSION_DATE "2022-05-05"
+#define VERSION_TIME "22:00:11"
+
+#define VERSION_FULL 5.1.0.0
+#define VERSION_MAJOR 5
+#define VERSION_MINOR 1
+#define VERSION_REVISION 0
+#define VERSION_BUILD 0
+
+#define VERSION_STRING_FILE "5.1.0.0"
+#define VERSION_STRING_PRODUCT "5.1.0 Nano's Net Unlocked"
