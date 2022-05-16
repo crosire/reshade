cffa0cc5ffbe6a989f71f5e3de30b1f4139f9eb8
crosire <crosiredev@gmail.com>
2022-05-16 20:33:32 +0200

    Fix incorrect function signature for "UnMapAndLoad"

--- setup/Utilities/PEInfo.cs
+++ setup/Utilities/PEInfo.cs
@@ -7,11 +7,17 @@ namespace ReShade.Setup.Utilities
 {
 	public unsafe class PEInfo
 	{
-		public enum BinaryType : ushort
+		public enum BinaryType : UInt16
 		{
 			IMAGE_FILE_MACHINE_UNKNOWN = 0x0,
 			IMAGE_FILE_MACHINE_I386 = 0x14c,
 			IMAGE_FILE_MACHINE_AMD64 = 0x8664,
+		}
+
+		public enum ImageDirectory : UInt16
+		{
+			IMAGE_DIRECTORY_ENTRY_EXPORT = 0,
+			IMAGE_DIRECTORY_ENTRY_IMPORT = 1,
 		}
 
 		[StructLayout(LayoutKind.Sequential)]
@@ -22,73 +28,74 @@ namespace ReShade.Setup.Utilities
 			public IntPtr MappedAddress;
 			public IntPtr FileHeader;
 			public IntPtr LastRvaSection;
-			public uint NumberOfSections;
+			public UInt32 NumberOfSections;
 			public IntPtr Sections;
-			public uint Characteristics;
-			public ushort fSystemImage;
-			public ushort fDOSImage;
-			public ushort fReadOnly;
-			public ushort Version;
+			public UInt32 Characteristics;
+			public UInt16 fSystemImage;
+			public UInt16 fDOSImage;
+			public UInt16 fReadOnly;
+			public UInt16 Version;
 			public IntPtr Flink;
 			public IntPtr BLink;
-			public uint SizeOfImage;
+			public UInt32 SizeOfImage;
 		}
-		[StructLayout(LayoutKind.Explicit)]
+
+		[StructLayout(LayoutKind.Sequential)]
 		private struct IMAGE_NT_HEADERS
 		{
-			[FieldOffset(0)]
-			public uint Signature;
-			[FieldOffset(4)]
+			public UInt32 Signature;
 			public IMAGE_FILE_HEADER FileHeader;
 		}
+
 		[StructLayout(LayoutKind.Sequential)]
 		private struct IMAGE_FILE_HEADER
 		{
 			public BinaryType Machine;
-			public ushort NumberOfSections;
-			public uint TimeDateStamp;
-			public uint PointerToSymbolTable;
-			public uint NumberOfSymbols;
-			public ushort SizeOfOptionalHeader;
-			public ushort Characteristics;
+			public UInt16 NumberOfSections;
+			public UInt32 TimeDateStamp;
+			public UInt32 PointerToSymbolTable;
+			public UInt32 NumberOfSymbols;
+			public UInt16 SizeOfOptionalHeader;
+			public UInt16 Characteristics;
 		}
+
 		[StructLayout(LayoutKind.Explicit)]
 		private struct IMAGE_IMPORT_DESCRIPTOR
 		{
-			#region union
 			[FieldOffset(0)]
-			public uint Characteristics;
+			public UInt32 Characteristics;
 			[FieldOffset(0)]
-			public uint OriginalFirstThunk;
-			#endregion
-
+			public UInt32 OriginalFirstThunk;
 			[FieldOffset(4)]
-			public uint TimeDateStamp;
+			public UInt32 TimeDateStamp;
 			[FieldOffset(8)]
-			public uint ForwarderChain;
+			public UInt32 ForwarderChain;
 			[FieldOffset(12)]
-			public uint Name;
+			public UInt32 Name;
 			[FieldOffset(16)]
-			public uint FirstThunk;
+			public UInt32 FirstThunk;
 		}
 
-		[DllImport("dbghelp.dll"), SuppressUnmanagedCodeSecurity]
-		private static extern void* ImageDirectoryEntryToData(void* pBase, bool mappedAsImage, ushort directoryEntry, out uint size);
-		[DllImport("dbghelp.dll"), SuppressUnmanagedCodeSecurity]
-		private static extern IntPtr ImageRvaToVa(IntPtr pNtHeaders, IntPtr pBase, uint rva, IntPtr pLastRvaSection);
-		[DllImport("imagehlp.dll"), SuppressUnmanagedCodeSecurity]
-		private static extern bool MapAndLoad(string imageName, string dllPath, out LOADED_IMAGE loadedImage, bool dotDll, bool readOnly);
-		[DllImport("imagehlp.dll"), SuppressUnmanagedCodeSecurity]
-		private static extern bool UnMapAndLoad(LOADED_IMAGE loadedImage);
+		[DllImport("imagehlp.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool MapAndLoad([In, MarshalAs(UnmanagedType.LPStr)] string imageName, [In, MarshalAs(UnmanagedType.LPStr)] string dllPath, [Out] out LOADED_IMAGE loadedImage, [In, MarshalAs(UnmanagedType.Bool)] bool dotDll, [In, MarshalAs(UnmanagedType.Bool)] bool readOnly);
+		[DllImport("imagehlp.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool UnMapAndLoad([In] ref LOADED_IMAGE loadedImage);
+
+		[DllImport("dbghelp.dll")]
+		private static extern IntPtr ImageRvaToVa([In] IntPtr pNtHeaders, [In] IntPtr pBase, [In] uint rva, [In] IntPtr pLastRvaSection);
+		[DllImport("dbghelp.dll")]
+		private static extern IntPtr ImageDirectoryEntryToData([In] IntPtr pBase, [In, MarshalAs(UnmanagedType.U1)] bool mappedAsImage, [In] ImageDirectory directoryEntry, [Out] out uint size);
 
 		// Adapted from http://stackoverflow.com/a/4696857/2055880
 		public PEInfo(string path)
 		{
 			var modules = new List<string>();
 
-			if (MapAndLoad(path, null, out LOADED_IMAGE image, true, true) && image.MappedAddress != IntPtr.Zero)
+			if (MapAndLoad(path, null, out LOADED_IMAGE image, true, true))
 			{
-				var imports = (IMAGE_IMPORT_DESCRIPTOR*)ImageDirectoryEntryToData((void*)image.MappedAddress, false, 1, out uint size);
+				var imports = (IMAGE_IMPORT_DESCRIPTOR*)ImageDirectoryEntryToData(image.MappedAddress, false, ImageDirectory.IMAGE_DIRECTORY_ENTRY_IMPORT, out uint size);
 
 				if (imports != null)
 				{
@@ -102,7 +109,7 @@ namespace ReShade.Setup.Utilities
 
 				Type = ((IMAGE_NT_HEADERS*)image.FileHeader)->FileHeader.Machine;
 
-				UnMapAndLoad(image);
+				UnMapAndLoad(ref image);
 			}
 
 			Modules = modules;
