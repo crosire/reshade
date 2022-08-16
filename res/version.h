55132d7a9a8a308beffd7f5f7ce2c4498d633fec
Zayveeo5e <37855671+Zayveeo5e@users.noreply.github.com>
2022-08-15 20:18:58 -0500

    Update version

--- res/version.h
+++ res/version.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#define VERSION_DATE "2022-05-05"
-#define VERSION_TIME "22:00:11"
+#define VERSION_DATE "2022-08-15"
+#define VERSION_TIME "00:00:00"
 
-#define VERSION_FULL 5.2.1.0
+#define VERSION_FULL 5.3.0.0
 #define VERSION_MAJOR 5
-#define VERSION_MINOR 2
-#define VERSION_REVISION 1
+#define VERSION_MINOR 3
+#define VERSION_REVISION 0
 #define VERSION_BUILD 0
 
-#define VERSION_STRING_FILE "5.2.1.0"
-#define VERSION_STRING_PRODUCT "5.1.0 Nano's Net Unlocked"
+#define VERSION_STRING_FILE "5.3.0.0"
+#define VERSION_STRING_PRODUCT "5.3.0 Nano's Net Unlocked"
