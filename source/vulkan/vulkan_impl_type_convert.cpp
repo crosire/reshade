5569e2f91e10edd292fe5d4eadeaedb7e025b904
crosire <crosiredev@gmail.com>
2022-12-11 23:50:20 +0100

    Add format conversion for "VK_FORMAT_A8B8G8R8_[...]_PACK32"

--- source/vulkan/vulkan_impl_type_convert.cpp
+++ source/vulkan/vulkan_impl_type_convert.cpp
@@ -283,10 +283,13 @@ auto reshade::vulkan::convert_format(VkFormat vk_format, const VkComponentMappin
 	case VK_FORMAT_R8G8_SNORM:
 		return api::format::r8g8_snorm;
 	case VK_FORMAT_R8G8B8A8_UINT:
+	case VK_FORMAT_A8B8G8R8_UINT_PACK32:
 		return api::format::r8g8b8a8_uint;
 	case VK_FORMAT_R8G8B8A8_SINT:
+	case VK_FORMAT_A8B8G8R8_SINT_PACK32:
 		return api::format::r8g8b8a8_sint;
 	case VK_FORMAT_R8G8B8A8_UNORM:
+	case VK_FORMAT_A8B8G8R8_UNORM_PACK32:
 		if (components != nullptr &&
 			components->r == VK_COMPONENT_SWIZZLE_R &&
 			components->g == VK_COMPONENT_SWIZZLE_G &&
@@ -295,6 +298,7 @@ auto reshade::vulkan::convert_format(VkFormat vk_format, const VkComponentMappin
 			return api::format::r8g8b8x8_unorm;
 		return api::format::r8g8b8a8_unorm;
 	case VK_FORMAT_R8G8B8A8_SRGB:
+	case VK_FORMAT_A8B8G8R8_SRGB_PACK32:
 		if (components != nullptr &&
 			components->r == VK_COMPONENT_SWIZZLE_R &&
 			components->g == VK_COMPONENT_SWIZZLE_G &&
@@ -303,6 +307,7 @@ auto reshade::vulkan::convert_format(VkFormat vk_format, const VkComponentMappin
 			return api::format::r8g8b8x8_unorm_srgb;
 		return api::format::r8g8b8a8_unorm_srgb;
 	case VK_FORMAT_R8G8B8A8_SNORM:
+	case VK_FORMAT_A8B8G8R8_SNORM_PACK32:
 		return api::format::r8g8b8a8_snorm;
 	case VK_FORMAT_B8G8R8A8_UNORM:
 		if (components != nullptr &&
