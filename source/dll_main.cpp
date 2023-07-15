786d7477442c8dac36b97e2f51654dfda6fcc032
crosire <crosiredev@gmail.com>
2023-07-15 03:21:49 +0200

    Log full application command-line

--- source/dll_main.cpp
+++ source/dll_main.cpp
@@ -188,7 +188,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID)
 #else
 				"(64-bit) "
 #endif
-				"loaded from " << g_reshade_dll_path << " into " << g_target_executable_path << " ...";
+				"loaded from " << g_reshade_dll_path << " into " << static_cast<const wchar_t *>(GetCommandLineW()) << " ...";
 
 			// Check if another ReShade instance was already loaded into the process
 			if (HMODULE modules[1024]; K32EnumProcessModules(GetCurrentProcess(), modules, sizeof(modules), &fdwReason)) // Use kernel32 variant which is available in DllMain
