f313872642bc2cf0ac8cdbdd8c8eda05dc2ec16a
TheIronWolfModding <526473+gb96@users.noreply.github.com>
2023-10-08 13:39:09 -0700

    WIP

--- source/vulkan/vulkan_impl_command_list.cpp
+++ source/vulkan/vulkan_impl_command_list.cpp
@@ -377,12 +377,12 @@ void reshade::vulkan::command_list_impl::resolve_depth_stencil(api::resource src
 	dst_subresource.mipLevel = 0; // TODO_TIW: this assumes subresource 0
 	dst_subresource.arrayLayer = 0;  // TODO_TIW: this assumes subresource 0
 
-	VkImageSubresourceLayers dst_subresource_layers = {
+	const VkImageSubresourceLayers dst_subresource_layers = {
 	  dst_subresource.aspectMask,
 	  dst_subresource.mipLevel,
 	  dst_subresource.arrayLayer, 1 };
 
-	VkImageSubresourceLayers src_subresource_layers = {
+	const VkImageSubresourceLayers src_subresource_layers = {
 	  src_subresource.aspectMask,
 	  src_subresource.mipLevel,
 	  src_subresource.arrayLayer, 1 };
@@ -392,52 +392,78 @@ void reshade::vulkan::command_list_impl::resolve_depth_stencil(api::resource src
 	VkImageBlit blit_info;
 	blit_info.dstSubresource = dst_subresource_layers;
 	blit_info.srcSubresource = src_subresource_layers;
-
 	blit_info.dstOffsets[0] = VkOffset3D { 0, 0, 0 };
 	blit_info.dstOffsets[1] = VkOffset3D { int32_t(dst_extent.width), int32_t(dst_extent.height), 1 };
 	blit_info.srcOffsets[0] = VkOffset3D { 0, 0, 0 };
 	blit_info.srcOffsets[1] = VkOffset3D { int32_t(src_extent.width), int32_t(src_extent.height), 1 };
 
-	VkImageResolve region;
-	region.srcSubresource = blit_info.srcSubresource;
-	region.srcOffset = blit_info.srcOffsets[0];
-	region.dstSubresource = intermediate ? blit_info.srcSubresource : blit_info.dstSubresource;
-	region.dstOffset = intermediate ? blit_info.srcOffsets[0] : blit_info.dstOffsets[0];
-	region.extent = srcCopyExtent;
+	const VkExtent3D src_copy_extent = {
+		uint32_t(blit_info.srcOffsets[1].x - blit_info.srcOffsets[0].x),
+		uint32_t(blit_info.srcOffsets[1].y - blit_info.srcOffsets[0].y),
+		uint32_t(blit_info.srcOffsets[1].z - blit_info.srcOffsets[0].z) };
 
-	VkImageSubresourceRange range;
-	range.aspectMask = layers.aspectMask;
-	range.baseMipLevel = layers.mipLevel;
-	range.levelCount = 1;
-	range.baseArrayLayer = layers.baseArrayLayer;
-	range.layerCount = layers.layerCount;
+	const VkExtent3D dst_copy_extent = {
+		uint32_t(blit_info.dstOffsets[1].x - blit_info.dstOffsets[0].x),
+		uint32_t(blit_info.dstOffsets[1].y - blit_info.dstOffsets[0].y),
+		uint32_t(blit_info.dstOffsets[1].z - blit_info.dstOffsets[0].z) };
 
-	info.subresourceRange = vk::makeSubresourceRange(dstSubresources);
+	VkImageResolve resolve_region;
+	resolve_region.srcSubresource = blit_info.srcSubresource;
+	resolve_region.srcOffset = blit_info.srcOffsets[0];
+	resolve_region.dstSubresource = blit_info.srcSubresource;
+	resolve_region.dstOffset = blit_info.srcOffsets[0];
+	resolve_region.extent = src_copy_extent;
 
-	if (m_vkd->vkCreateImageView(m_vkd->device(), &info, nullptr, &m_dstImageView))
-		throw DxvkError("DxvkMetaResolveViews: Failed to create destination view");
-
-	info.image = srcImage->handle();
-	info.subresourceRange = vk::makeSubresourceRange(srcSubresources);
-
-	if (m_vkd->vkCreateImageView(m_vkd->device(), &info, nullptr, &m_srcImageView))
-		throw DxvkError("DxvkMetaResolveViews: Failed to create source view");
-
-
+	VkImageSubresourceRange dst_subresource_range;
+	dst_subresource_range.aspectMask = resolve_region.dstSubresource.aspectMask;
+	dst_subresource_range.baseMipLevel = resolve_region.dstSubresource.mipLevel;
+	dst_subresource_range.levelCount = 1;
+	dst_subresource_range.baseArrayLayer = resolve_region.dstSubresource.baseArrayLayer;
+	dst_subresource_range.layerCount = resolve_region.dstSubresource.layerCount;
+	/*subresource_range.aspectMask = dst_subresource_layers.aspectMask;
+	subresource_range.baseMipLevel = dst_subresource_layers.mipLevel;
+	subresource_range.levelCount = 1;
+	subresource_range.baseArrayLayer = dst_subresource_layers.baseArrayLayer;
+	subresource_range.layerCount = dst_subresource_layers.layerCount;*/
 
 	VkImageViewUsageCreateInfo usage_info = { VK_STRUCTURE_TYPE_IMAGE_VIEW_USAGE_CREATE_INFO };
 	usage_info.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT; // TODO_TIW?
 
-	VkImageViewCreateInfo info = { VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO, &usage_info };
-	info.image = dst.handle;
-	info.viewType = VK_IMAGE_VIEW_TYPE_2D_ARRAY;
-	info.format = VK_FORMAT_D32_SFLOAT_S8_UINT; // TODO_TIW no idea how to get to it.
+	VkImageViewCreateInfo img_create_info = { VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO, &usage_info };
+	img_create_info.image = dst.handle;
+	img_create_info.viewType = VK_IMAGE_VIEW_TYPE_2D_ARRAY;
+	img_create_info.format = VK_FORMAT_D32_SFLOAT_S8_UINT; // TODO_TIW no idea how to get to it.
+	img_create_info.subresourceRange = dst_subresource_range;
+
+	VkImageView dst_img_view;
+	VkImageView src_img_view;
+	if (vk.CreateImageView(_device_impl->_orig,  &img_create_info, nullptr, &dst_img_view)) {
+		assert(false);
+		return;
+	}
+	VkImageSubresourceRange src_subresource_range;
+
+	src_subresource_range.aspectMask = resolve_region.srcSubresource.aspectMask;
+	src_subresource_range.baseMipLevel = resolve_region.srcSubresource.mipLevel;
+	src_subresource_range.levelCount = 1;
+	src_subresource_range.baseArrayLayer = resolve_region.srcSubresource.baseArrayLayer;
+	src_subresource_range.layerCount = resolve_region.srcSubresource.layerCount;
+
+	img_create_info.image = src.handle;
+	img_create_info.subresourceRange = src_subresource_range;
+
+	VkImageView src_img_view;
+	if (vk.CreateImageView(_device_impl->_orig, &img_create_info, nullptr, &src_img_view)) {
+		assert(false);
+		return;
+	}
+
 
 	VkRenderingAttachmentInfo depthAttachment = { VK_STRUCTURE_TYPE_RENDERING_ATTACHMENT_INFO };
-	depthAttachment.imageView = views->getSrcView();
+	depthAttachment.imageView = src_img_view;
 	depthAttachment.imageLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL; // TODO_TIW:
 	depthAttachment.resolveMode = VK_RESOLVE_MODE_AVERAGE_BIT;
-	depthAttachment.resolveImageView = views->getDstView();
+	depthAttachment.resolveImageView = dst_img_view;
 	depthAttachment.resolveImageLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
 	depthAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_LOAD;
 	depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
