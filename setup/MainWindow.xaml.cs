aece9ccb2632c50d2c08b04997142884e2d66016
crosire <crosiredev@gmail.com>
2023-12-08 16:15:04 +0100

    Fix uninstall failing when invoked through the command-line

--- setup/MainWindow.xaml.cs
+++ setup/MainWindow.xaml.cs
@@ -705,12 +705,6 @@ namespace ReShade.Setup
 
 			configPath = Path.Combine(basePath, "ReShade.ini");
 
-			if (operation == InstallOperation.Uninstall)
-			{
-				UninstallStep_UninstallReShadeModule();
-				return;
-			}
-
 			var isReShade = false;
 
 			if (targetApi == Api.Vulkan || targetOpenXR)
@@ -718,7 +712,7 @@ namespace ReShade.Setup
 				var moduleName = is64Bit ? "ReShade64" : "ReShade32";
 				modulePath = Path.Combine(commonPath, moduleName, moduleName + ".dll");
 
-				if (operation != InstallOperation.Update && operation != InstallOperation.Modify && File.Exists(configPath))
+				if (operation == InstallOperation.Default && File.Exists(configPath))
 				{
 					if (isHeadless)
 					{
@@ -765,7 +759,7 @@ namespace ReShade.Setup
 
 				modulePath = Path.Combine(basePath, modulePath);
 
-				if (operation != InstallOperation.Update && operation != InstallOperation.Modify && ModuleExists(modulePath, out isReShade))
+				if (operation == InstallOperation.Default && ModuleExists(modulePath, out isReShade))
 				{
 					if (isReShade)
 					{
@@ -794,7 +788,7 @@ namespace ReShade.Setup
 			{
 				string conflictingModulePath = Path.Combine(basePath, conflictingModuleName);
 
-				if (operation != InstallOperation.Update && operation != InstallOperation.Modify && ModuleExists(conflictingModulePath, out isReShade) && isReShade)
+				if (operation == InstallOperation.Default && ModuleExists(conflictingModulePath, out isReShade) && isReShade)
 				{
 					if (isHeadless)
 					{
@@ -813,7 +807,14 @@ namespace ReShade.Setup
 				}
 			}
 
-			InstallStep_InstallReShadeModule();
+			if (operation != InstallOperation.Uninstall)
+			{
+				InstallStep_InstallReShadeModule();
+			}
+			else
+			{
+				UninstallStep_UninstallReShadeModule();
+			}
 		}
 		void InstallStep_InstallReShadeModule()
 		{
