be90fd813529711534598245bf64b6667c4d0c28
cot6 <cot6dev@mx.ovo.run>
2024-02-02 13:43:36 +0900

    Use sleep with constants to fix easily unstable framerate issue

--- examples/01-framerate_limit/framerate_limit_addon.cpp
+++ examples/01-framerate_limit/framerate_limit_addon.cpp
@@ -11,8 +11,7 @@
 static bool s_enabled = false;
 static uint32_t s_fps_limit = 0;
 static bool s_started = false;
-static uint64_t s_num_frames = 0;
-static std::chrono::high_resolution_clock::time_point s_start_point;
+static std::chrono::high_resolution_clock::time_point s_last_point;
 
 static void on_present(reshade::api::command_queue *, reshade::api::swapchain *, const reshade::api::rect *, const reshade::api::rect *, uint32_t, const reshade::api::rect *)
 {
@@ -22,32 +21,16 @@ static void on_present(reshade::api::command_queue *, reshade::api::swapchain *,
 	if (!s_started)
 	{
 		s_started = true;
-		s_num_frames = 0;
-		s_start_point = std::chrono::high_resolution_clock::now();
+		s_last_point = std::chrono::high_resolution_clock::now();
 	}
 
 	const auto time_per_frame = std::chrono::high_resolution_clock::duration(std::chrono::seconds(1)) / s_fps_limit;
+	const auto next_point = s_last_point + time_per_frame;
 
-	const auto cur_frame_point = s_start_point + s_num_frames++ * time_per_frame;
-	const auto next_frame_point = cur_frame_point + time_per_frame;
+	while (next_point > std::chrono::high_resolution_clock::now())
+		std::this_thread::sleep_for(std::chrono::high_resolution_clock::duration(std::chrono::milliseconds(1)));
 
-	while (true)
-	{
-		const auto now = std::chrono::high_resolution_clock::now();
-		if (now < cur_frame_point || now > next_frame_point)
-		{
-			s_started = false;
-			break;
-		}
-
-		const auto time_remaining = (next_frame_point - now);
-		if (time_remaining <= std::chrono::milliseconds(1))
-		{
-			break;
-		}
-
-		std::this_thread::sleep_for(time_remaining - std::chrono::milliseconds(1));
-	}
+	s_last_point = next_point;
 }
 
 static void draw_settings(reshade::api::effect_runtime *)
