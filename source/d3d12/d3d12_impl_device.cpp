4b5f082e67aaf2d6ed6c7537e68c5a03c0a5dd68
crosire <crosiredev@gmail.com>
2024-05-10 22:27:01 +0200

    Always set directly indexed heap flags on D3D12 root signature creation to keep compatibility when application uses the feature

--- source/d3d12/d3d12_impl_device.cpp
+++ source/d3d12/d3d12_impl_device.cpp
@@ -1397,11 +1397,18 @@ bool reshade::d3d12::device_impl::create_pipeline_layout(uint32_t param_count, c
 	assert(D3D12SerializeRootSignature != nullptr);
 
 	D3D12_ROOT_SIGNATURE_DESC internal_desc = {};
-	internal_desc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 	internal_desc.NumParameters = static_cast<uint32_t>(internal_params.size());
 	internal_desc.pParameters = internal_params.data();
 	internal_desc.NumStaticSamplers = static_cast<uint32_t>(internal_static_samplers.size());
 	internal_desc.pStaticSamplers = internal_static_samplers.data();
+	internal_desc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+	if (std::pair<D3D12_FEATURE_DATA_SHADER_MODEL, D3D12_FEATURE_DATA_D3D12_OPTIONS> options;
+		SUCCEEDED(_orig->CheckFeatureSupport(D3D12_FEATURE_SHADER_MODEL, &options.first, sizeof(options.first))) &&
+		SUCCEEDED(_orig->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS, &options.second, sizeof(options.second))) &&
+		options.first.HighestShaderModel >= D3D_SHADER_MODEL_6_6 &&
+		options.second.ResourceBindingTier >= D3D12_RESOURCE_BINDING_TIER_3)
+		internal_desc.Flags |= D3D12_ROOT_SIGNATURE_FLAG_CBV_SRV_UAV_HEAP_DIRECTLY_INDEXED | D3D12_ROOT_SIGNATURE_FLAG_SAMPLER_HEAP_DIRECTLY_INDEXED;
 
 	com_ptr<ID3DBlob> signature_blob, error_blob;
 	com_ptr<ID3D12RootSignature> signature;
