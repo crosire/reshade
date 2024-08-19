77a919b1fc0e65b88e8b5bdadeb914efbfcf66ff
cot6 <cot6dev@mx.ovo.run>
2024-08-19 17:54:25 +0900

    Invalid STL handling if all effects are required to reload

--- source/runtime.cpp
+++ source/runtime.cpp
@@ -3800,11 +3800,16 @@ void reshade::runtime::update_effects()
 		assert(_reload_required_effects.back() <= _effects.size());
 
 		if (_reload_required_effects.back() < _effects.size())
+		{
 			reload_effect(_reload_required_effects.back());
+			_reload_required_effects.pop_back();
+		}
 		else
+		{
 			reload_effects();
+			assert(_reload_required_effects.empty());
+		}
 
-		_reload_required_effects.pop_back();
 	}
 
 	if (_reload_remaining_effects == 0)
