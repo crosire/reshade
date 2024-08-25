crosire/reshade

commit f3ca9efb7654842d5aeb504c3080b9e881079f91

Replace STL file streams with classic C file functions
This reduces the binary size quite a lot and gets rid of a bunch of unused locale code

source/ini_file.cpp
@@ -4,8 +4,6 @@
  */
 
 #include "ini_file.hpp"
-#include <fstream>
-#include <sstream>
 #include <shared_mutex>
 #include <cctype> // std::toupper
 #include <cassert>
@@ -35,21 +33,23 @@ bool ini_file::load()
 	// Clear when file does not exist too
 	_sections.clear();
 
-	std::ifstream file(_path);
-	if (!file)
+	FILE *const file = _wfsopen(_path.c_str(), L"r", SH_DENYWR);
+	if (file == nullptr)
 		return false;
 
 	_modified = false;
 	_modified_at = modified_at;
 
-	// Remove BOM
-	if (file.get() != utf8::bom[0] || file.get() != utf8::bom[1] || file.get() != utf8::bom[2])
-		file.seekg(0, std::ios::beg);
+	// Remove BOM (0xefbbbf means 0xfeff)
+	if (fgetc(file) != utf8::bom[0] || fgetc(file) != utf8::bom[1] || fgetc(file) != utf8::bom[2])
+		fseek(file, 0, SEEK_SET);
 
-	std::string line, section;
-	while (std::getline(file, line))
+	std::string section;
+
+	char line_data[2048];
+	while (fgets(line_data, sizeof(line_data), file))
 	{
-		line = trim(line);
+		const std::string_view line = trim(line_data, " \t\r\n");
 
 		if (line.empty() || line[0] == ';' || line[0] == '/' || line[0] == '#')
 			continue;
@@ -65,17 +65,17 @@ bool ini_file::load()
 		const size_t assign_index = line.find('=');
 		if (assign_index != std::string::npos)
 		{
-			const std::string key = trim(line.substr(0, assign_index));
-			const std::string value = trim(line.substr(assign_index + 1));
+			const std::string_view key = trim(line.substr(0, assign_index));
+			const std::string_view value = trim(line.substr(assign_index + 1));
 
 			if (value.empty())
 			{
-				_sections[section].insert({ key, {} });
+				_sections[section].insert({ std::string(key), {} });
 				continue;
 			}
 
 			// Append to key if it already exists
-			ini_file::value_type &elements = _sections[section][key];
+			ini_file::value_type &elements = _sections[section][std::string(key)];
 			for (size_t offset = 0, base = 0, len = value.size(); offset <= len;)
 			{
 				// Treat ",," as an escaped comma and only split on single ","
@@ -104,9 +104,11 @@ bool ini_file::load()
 		}
 		else
 		{
-			_sections[section].insert({ line, {} });
+			_sections[section].insert({ std::string(line), {} });
 		}
 	}
+
+	fclose(file);
 
 	return true;
 }
@@ -123,7 +125,7 @@ bool ini_file::save()
 	if (!ec && (modified_at - _modified_at) > std::chrono::seconds(2))
 		return false; // File exists and was modified on disk and therefore may have different data, so cannot save
 
-	std::stringstream data;
+	std::string data;
 	std::vector<std::string> section_names, key_names;
 
 	section_names.reserve(_sections.size());
@@ -156,11 +158,11 @@ bool ini_file::save()
 
 			// Empty section should have been sorted to the top, so do not need to append it before keys
 			if (!section_name.empty())
-				data << '[' << section_name << ']' << '\n';
+				data += '[' + section_name + ']' + '\n';
 
 			for (const std::string &key_name : key_names)
 			{
-				data << key_name << '=';
+				data += key_name + '=';
 
 				if (const ini_file::value_type &elements = keys.at(key_name); !elements.empty())
 				{
@@ -184,28 +186,25 @@ bool ini_file::save()
 						value.pop_back();
 					}
 
-					data << value;
+					data += value;
 				}
 
-				data << '\n';
+				data += '\n';
 			}
 
-			data << '\n';
+			data += '\n';
 		}
 	}
 
-	std::ofstream file(_path);
-	if (!file)
+	FILE *const file = _wfsopen(_path.c_str(), L"w", SH_DENYWR);
+	if (file == nullptr)
 		return false;
-
-	const std::string str = data.str();
+	const size_t file_size_written = fwrite(data.data(), 1, data.size(), file);
+	fclose(file);
+	if (file_size_written != data.size())
+		return false;
 
 	// Flush stream to disk before updating last write time
-	const bool fail = !(file.write(str.data(), str.size()) && file.flush());
-	file.close();
-	if (fail)
-		return false;
-
 	_modified_at = std::filesystem::last_write_time(_path, ec);
 
 	assert(!ec && std::filesystem::file_size(_path, ec) > 0);

source/effect_preprocessor.cpp
@@ -5,8 +5,8 @@
 
 #include "effect_lexer.hpp"
 #include "effect_preprocessor.hpp"
+#include <cstdio> // fclose, fopen, fread, fseek
 #include <cassert>
-#include <fstream>
 #include <algorithm> // std::find_if
 
 #ifndef _WIN32
@@ -61,24 +61,29 @@ static const int s_precedence_lookup[] = {
 	11, 11, 11, 11 // unary operators
 };
 
-static bool read_file(const std::filesystem::path &path, std::string &data)
+static bool read_file(const std::filesystem::path &path, std::string &file_data)
 {
-	std::ifstream file(path, std::ios::binary);
-	if (!file)
-		return false;
-
 	// Read file contents into memory
-	std::error_code ec;
-	const uintmax_t file_size = std::filesystem::file_size(path, ec);
-	if (ec)
+#ifndef _WIN32
+	FILE *const file = fopen(path.c_str(), "rb");
+#else
+	FILE *const file = _wfsopen(path.c_str(), L"rb", SH_DENYWR);
+#endif
+	if (file == nullptr)
 		return false;
 
-	std::string file_data(static_cast<size_t>(file_size + 1), '\0');
-	if (!file.read(file_data.data(), file_size))
-		return false;
+	fseek(file, 0, SEEK_END);
+	const size_t file_size = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	file_data.resize(file_size + 1, '\0'); // One additional character at the end for new line feed set below
+	const size_t file_size_read = fread(file_data.data(), 1, file_size, file);
 
 	// No longer need to have a handle open to the file, since all data was read, so can safely close it
-	file.close();
+	fclose(file);
+
+	if (file_size_read != file_size)
+		return false;
 
 	// Append a new line feed to the end of the input string to avoid issues with parsing
 	file_data.back() = '\n';
@@ -90,7 +95,6 @@ static bool read_file(const std::filesystem::path &path, std::string &data)
 		static_cast<unsigned char>(file_data[2]) == 0xbf)
 		file_data.erase(0, 3);
 
-	data = std::move(file_data);
 	return true;
 }
 

source/ini_file.hpp
@@ -14,11 +14,10 @@ extern std::filesystem::path g_reshade_dll_path;
 extern std::filesystem::path g_reshade_base_path;
 extern std::filesystem::path g_target_executable_path;
 
-inline std::string trim(std::string str, const char chars[] = " \t")
+inline std::string_view trim(std::string_view str, const char chars[] = " \t")
 {
-	str.erase(0, str.find_first_not_of(chars));
-	str.erase(str.find_last_not_of(chars) + 1);
-	return str;
+	const size_t first = str.find_first_not_of(chars);
+	return first != std::string_view::npos ? std::string_view(str.data() + first, str.find_last_not_of(chars) + 1 - first) : std::string_view();
 }
 
 class ini_file

source/runtime.cpp
@@ -20,7 +20,6 @@
 #include "reshade_api_object_impl.hpp"
 #include <set>
 #include <thread>
-#include <fstream>
 #include <cmath> // std::abs, std::fmod
 #include <cctype> // std::toupper
 #include <cwctype> // std::towlower
@@ -3010,15 +3009,16 @@ void reshade::runtime::load_textures()
 			continue;
 		}
 
-		std::error_code ec;
-		const uintmax_t file_size = std::filesystem::file_size(source_path, ec);
-
 		void *pixels = nullptr;
 		int width = 0, height = 1, depth = 1, channels = 0;
 		const bool is_floating_point_format = (tex.format == reshadefx::texture_format::r32f || tex.format == reshadefx::texture_format::rg32f || tex.format == reshadefx::texture_format::rgba32f);
 
-		if (auto file = std::ifstream(source_path, std::ios::binary))
+		if (FILE *const file = _wfsopen(source_path.c_str(), L"rb", SH_DENYNO))
 		{
+			fseek(file, 0, SEEK_END);
+			const size_t file_size = ftell(file);
+			fseek(file, 0, SEEK_SET);
+
 			if (source_path.extension() == L".cube")
 			{
 				if (!is_floating_point_format)
@@ -3032,13 +3032,15 @@ void reshade::runtime::load_textures()
 				float domain_max[3] = { 1.0f, 1.0f, 1.0f };
 
 				// Read header information
-				std::string line;
-				while (std::getline(file, line))
+				char line_data[1024];
+				while (fgets(line_data, sizeof(line_data), file))
 				{
+					const std::string_view line(line_data);
+
 					if (line.empty() || line[0] == '#')
 						continue; // Skip lines with comments
 
-					char *p = line.data();
+					char *p = line_data;
 
 					if (line.rfind("TITLE", 0) == 0)
 						continue; // Skip optional line with title
@@ -3078,7 +3080,7 @@ void reshade::runtime::load_textures()
 					}
 
 					// Line has no known keyword, so assume this is where the table data starts and roll back a line to continue reading that below
-					file.seekg(-static_cast<std::streampos>(line.size() + 1), std::ios::cur);
+					fseek(file, -static_cast<long>(line.size() + 1), SEEK_CUR);
 					break;
 				}
 
@@ -3086,12 +3088,15 @@ void reshade::runtime::load_textures()
 				if (pixels != nullptr)
 				{
 					size_t index = 0;
-					while (std::getline(file, line) && (index + 4) <= (static_cast<size_t>(width) * static_cast<size_t>(height) * static_cast<size_t>(depth) * 4))
+
+					while (fgets(line_data, sizeof(line_data), file) && (index + 4) <= (static_cast<size_t>(width) * static_cast<size_t>(height) * static_cast<size_t>(depth) * 4))
 					{
+						const std::string_view line(line_data);
+
 						if (line.empty() || line[0] == '#')
 							continue; // Skip lines with comments
 
-						char *p = line.data();
+						char *p = line_data;
 
 						static_cast<float *>(pixels)[index++] = static_cast<float>(std::strtod(p, &p)) * (domain_max[0] - domain_min[0]) + domain_min[0];
 						static_cast<float *>(pixels)[index++] = static_cast<float>(std::strtod(p, &p)) * (domain_max[1] - domain_min[1]) + domain_min[1];
@@ -3103,22 +3108,25 @@ void reshade::runtime::load_textures()
 			else
 			{
 				// Read texture data into memory in one go since that is faster than reading chunk by chunk
-				std::vector<stbi_uc> file_data(static_cast<size_t>(file_size));
-				file.read(reinterpret_cast<char *>(file_data.data()), file_data.size());
-				file.close();
+				std::vector<stbi_uc> file_data(file_size);
+				const size_t file_size_read = fread(file_data.data(), 1, file_size, file);
+				fclose(file);
 
-				if (is_floating_point_format)
-					pixels = stbi_loadf_from_memory(file_data.data(), static_cast<int>(file_data.size()), &width, &height, &channels, STBI_rgb_alpha);
-				else if (stbi_dds_test_memory(file_data.data(), static_cast<int>(file_data.size())))
-					pixels = stbi_dds_load_from_memory(file_data.data(), static_cast<int>(file_data.size()), &width, &height, &depth, &channels, STBI_rgb_alpha);
-				else
-					pixels = stbi_load_from_memory(file_data.data(), static_cast<int>(file_data.size()), &width, &height, &channels, STBI_rgb_alpha);
+				if (file_size_read == file_size)
+				{
+					if (is_floating_point_format)
+						pixels = stbi_loadf_from_memory(file_data.data(), static_cast<int>(file_data.size()), &width, &height, &channels, STBI_rgb_alpha);
+					else if (stbi_dds_test_memory(file_data.data(), static_cast<int>(file_data.size())))
+						pixels = stbi_dds_load_from_memory(file_data.data(), static_cast<int>(file_data.size()), &width, &height, &depth, &channels, STBI_rgb_alpha);
+					else
+						pixels = stbi_load_from_memory(file_data.data(), static_cast<int>(file_data.size()), &width, &height, &channels, STBI_rgb_alpha);
+				}
 			}
 		}
 
-		if (ec || pixels == nullptr)
+		if (pixels == nullptr)
 		{
-			log::message(log::level::error, "Failed to load '%s' for texture '%s' with error code %d!", source_path.u8string().c_str(), tex.unique_name.c_str(), ec.value());
+			log::message(log::level::error, "Failed to load '%s' for texture '%s'!", source_path.u8string().c_str(), tex.unique_name.c_str());
 			_last_reload_successful = false;
 			continue;
 		}
@@ -3610,18 +3618,18 @@ bool reshade::runtime::load_effect_cache(const std::string &id, const std::strin
 	std::filesystem::path path = g_reshade_base_path / _effect_cache_path;
 	path /= std::filesystem::u8path("reshade-" + id + '.' + type);
 
-	std::ifstream file(path, std::ios::binary);
-	if (!file)
+	FILE *const file = _wfsopen(path.c_str(), L"rb", SH_DENYNO);
+	if (file == nullptr)
 		return false;
 
-	std::error_code ec;
-	const uintmax_t file_size = std::filesystem::file_size(path, ec);
-	if (ec)
-		return false;
+	fseek(file, 0, SEEK_END);
+	const size_t file_size = ftell(file);
+	fseek(file, 0, SEEK_SET);
 
-	data.resize(static_cast<size_t>(file_size), '\0');
-	file.read(data.data(), data.size());
-	return !file.fail();
+	data.resize(file_size, '\0');
+	const size_t file_size_read = fread(data.data(), 1, data.size(), file);
+	fclose(file);
+	return file_size_read == data.size();
 }
 bool reshade::runtime::save_effect_cache(const std::string &id, const std::string &type, const std::string &data) const
 {
@@ -3631,12 +3639,13 @@ bool reshade::runtime::save_effect_cache(const std::string &id, const std::strin
 	std::filesystem::path path = g_reshade_base_path / _effect_cache_path;
 	path /= std::filesystem::u8path("reshade-" + id + '.' + type);
 
-	std::ofstream file(path, std::ios::binary | std::ios::trunc);
-	if (!file)
+	FILE *const file = _wfsopen(path.c_str(), L"wb", SH_DENYNO);
+	if (file == nullptr)
 		return false;
 
-	file.write(data.data(), data.size());
-	return !file.fail();
+	const size_t file_size_written = fwrite(data.data(), 1, data.size(), file);
+	fclose(file);
+	return file_size_written == data.size();
 }
 void reshade::runtime::clear_effect_cache()
 {
@@ -4402,35 +4411,35 @@ void reshade::runtime::save_texture(const texture &tex)
 			// Default to a save failure unless it is reported to succeed below
 			bool save_success = false;
 
-			if (auto file = std::ofstream(screenshot_path, std::ios::binary | std::ios::trunc))
+			if (FILE *const file = _wfsopen(screenshot_path.c_str(), L"wb", SH_DENYNO))
 			{
 				const auto write_callback = [](void *context, void *data, int size) {
-					static_cast<std::ofstream *>(context)->write(static_cast<const char *>(data), size);
+					fwrite(data, 1, size, static_cast<FILE *>(context));
 				};
 
 				switch (_screenshot_format)
 				{
 				case 0:
-					save_success = stbi_write_bmp_to_func(write_callback, &file, width, height, 4, pixels.data()) != 0;
+					save_success = stbi_write_bmp_to_func(write_callback, file, width, height, 4, pixels.data()) != 0;
 					break;
 				case 1:
-				{
 #if 1
-					std::vector<uint8_t> encoded_data;
-					save_success = fpng::fpng_encode_image_to_memory(pixels.data(), width, height, 4, encoded_data);
-					write_callback(&file, encoded_data.data(), static_cast<int>(encoded_data.size()));
+					if (std::vector<uint8_t> encoded_data;
+						fpng::fpng_encode_image_to_memory(pixels.data(), width, height, 4, encoded_data))
+						save_success = fwrite(encoded_data.data(), 1, encoded_data.size(), file) == encoded_data.size();
 #else
-					save_success = stbi_write_png_to_func(write_callback, &file, width, height, 4, pixels.data(), 0) != 0;
+					save_success = stbi_write_png_to_func(write_callback, file, width, height, 4, pixels.data(), 0) != 0;
 #endif
 					break;
-				}
 				case 2:
-					save_success = stbi_write_jpg_to_func(write_callback, &file, width, height, 4, pixels.data(), _screenshot_jpeg_quality) != 0;
+					save_success = stbi_write_jpg_to_func(write_callback, file, width, height, 4, pixels.data(), _screenshot_jpeg_quality) != 0;
 					break;
 				}
 
-				if (!file)
+				if (ferror(file))
 					save_success = false;
+
+				fclose(file);
 			}
 
 			if (_last_screenshot_save_successful)
@@ -4934,35 +4943,35 @@ void reshade::runtime::save_screenshot(const std::string_view postfix)
 			// Default to a save failure unless it is reported to succeed below
 			bool save_success = false;
 
-			if (auto file = std::ofstream(screenshot_path, std::ios::binary | std::ios::trunc))
+			if (FILE *const file = _wfsopen(screenshot_path.c_str(), L"wb", SH_DENYNO))
 			{
 				const auto write_callback = [](void *context, void *data, int size) {
-					static_cast<std::ofstream *>(context)->write(static_cast<const char *>(data), size);
+					fwrite(data, 1, size, static_cast<FILE *>(context));
 				};
 
 				switch (_screenshot_format)
 				{
 				case 0:
-					save_success = stbi_write_bmp_to_func(write_callback, &file, _width, _height, comp, pixels.data()) != 0;
+					save_success = stbi_write_bmp_to_func(write_callback, file, _width, _height, comp, pixels.data()) != 0;
 					break;
 				case 1:
-				{
 #if 1
-					std::vector<uint8_t> encoded_data;
-					save_success = fpng::fpng_encode_image_to_memory(pixels.data(), _width, _height, comp, encoded_data);
-					write_callback(&file, encoded_data.data(), static_cast<int>(encoded_data.size()));
+					if (std::vector<uint8_t> encoded_data;
+						fpng::fpng_encode_image_to_memory(pixels.data(), _width, _height, comp, encoded_data))
+						save_success = fwrite(encoded_data.data(), 1, encoded_data.size(), file) == encoded_data.size();
 #else
-					save_success = stbi_write_png_to_func(write_callback, &file, _width, _height, comp, pixels.data(), 0) != 0;
+					save_success = stbi_write_png_to_func(write_callback, file, _width, _height, comp, pixels.data(), 0) != 0;
 #endif
 					break;
-				}
 				case 2:
-					save_success = stbi_write_jpg_to_func(write_callback, &file, _width, _height, comp, pixels.data(), _screenshot_jpeg_quality) != 0;
+					save_success = stbi_write_jpg_to_func(write_callback, file, _width, _height, comp, pixels.data(), _screenshot_jpeg_quality) != 0;
 					break;
 				}
 
-				if (!file)
+				if (ferror(file))
 					save_success = false;
+
+				fclose(file);
 			}
 
 			if (save_success)

source/runtime_gui.cpp
@@ -19,7 +19,6 @@
 #include "platform_utils.hpp"
 #include "fonts/forkawesome.inl"
 #include "fonts/glyph_ranges.hpp"
-#include <fstream>
 #include <cmath> // std::abs, std::ceil, std::floor
 #include <cctype> // std::tolower
 #include <cstdlib> // std::lldiv, std::strtol
@@ -3019,10 +3018,17 @@ void reshade::runtime::draw_gui_log()
 		if (filter_changed || _last_log_size != file_size)
 		{
 			_log_lines.clear();
-			std::ifstream log_file(log_path);
-			for (std::string line; std::getline(log_file, line);)
-				if (filter_text(line, _log_filter))
-					_log_lines.push_back(line);
+
+			if (FILE *const file = _wfsopen(log_path.c_str(), L"r", SH_DENYNO))
+			{
+				char line_data[2048];
+				while (fgets(line_data, sizeof(line_data), file))
+					if (filter_text(line_data, _log_filter))
+						_log_lines.push_back(line_data);
+
+				fclose(file);
+			}
+
 			_last_log_size = file_size;
 		}
 
@@ -4459,9 +4465,18 @@ void reshade::runtime::open_code_editor(editor_instance &instance) const
 	// Only update text if there is no undo history (in which case it can be assumed that the text is already up-to-date)
 	if (!instance.editor.is_modified() && !instance.editor.can_undo())
 	{
-		if (auto file = std::ifstream(instance.file_path))
+		if (FILE *const file = _wfsopen(instance.file_path.c_str(), L"rb", SH_DENYWR))
 		{
-			instance.editor.set_text(std::string(std::istreambuf_iterator<char>(file), {}));
+			fseek(file, 0, SEEK_END);
+			const size_t file_size = ftell(file);
+			fseek(file, 0, SEEK_SET);
+
+			std::string text(file_size, '\0');
+			fread(text.data(), 1, file_size, file);
+
+			fclose(file);
+
+			instance.editor.set_text(text);
 			instance.editor.set_readonly(false);
 		}
 	}
@@ -4484,10 +4499,11 @@ void reshade::runtime::draw_code_editor(editor_instance &instance)
 			_input != nullptr && _input->is_key_pressed('S', true, false, false))))
 	{
 		// Write current editor text to file
-		if (auto file = std::ofstream(instance.file_path, std::ios::trunc))
+		if (FILE *const file = _wfsopen(instance.file_path.c_str(), L"wb", SH_DENYWR))
 		{
 			const std::string text = instance.editor.get_text();
-			file.write(text.data(), text.size());
+			fwrite(text.data(), 1, text.size(), file);
+			fclose(file);
 		}
 
 		if (!is_loading() && instance.effect_index < _effects.size())

commit 78c7797993e9e4b9c1baf9f0e2a532e9b88268ac

Fix long lines in INI files getting truncated
This was a regression introduced in commit f3ca9efb7654842d5aeb504c3080b9e881079f91, due to "fgets" being used with a fixed buffer size

source/ini_file.cpp
@@ -44,12 +44,19 @@ bool ini_file::load()
 	if (fgetc(file) != utf8::bom[0] || fgetc(file) != utf8::bom[1] || fgetc(file) != utf8::bom[2])
 		fseek(file, 0, SEEK_SET);
 
-	std::string section;
-
-	char line_data[2048];
-	while (fgets(line_data, sizeof(line_data), file))
+	std::string line_data, section;
+	line_data.resize(BUFSIZ);
+	while (fgets(line_data.data(), static_cast<int>(line_data.size() + 1), file))
 	{
-		const std::string_view line = trim(line_data, " \t\r\n");
+		size_t line_length;
+		while ((line_length = std::strlen(line_data.data())) == line_data.size())
+		{
+			line_data.resize(line_data.size() + BUFSIZ);
+			if (!fgets(line_data.data() + line_length, static_cast<int>(line_data.size() - line_length), file))
+				break;
+		}
+
+		const std::string_view line = trim(std::string_view(line_data.data(), line_length), " \t\r\n");
 
 		if (line.empty() || line[0] == ';' || line[0] == '/' || line[0] == '#')
 			continue;