crosire/reshade

commit acf87102daaa9b077847d28eb7ef802a9e1c85ac

Change texture loading to happen during effect creation
This way, when a single effect is reloaded, only textures used with that effect are reloaded as well, instead of all of them

source/runtime.cpp
@@ -2751,6 +2751,8 @@ bool reshade::runtime::create_effect(size_t effect_index)
 	effect.assembly.clear();
 #endif
 
+	load_textures(effect_index);
+
 	return true;
 }
 bool reshade::runtime::create_effect_sampler_state(const reshadefx::sampler_desc &info, api::sampler &sampler)
@@ -2875,12 +2877,14 @@ void reshade::runtime::destroy_effect(size_t effect_index)
 	// Do not clear effect here, since it is common to be reused immediately
 }
 
-void reshade::runtime::load_textures()
+void reshade::runtime::load_textures(size_t effect_index)
 {
 	for (texture &tex : _textures)
 	{
 		if (tex.resource == 0 || !tex.semantic.empty())
 			continue; // Ignore textures that are not created yet and those that are handled in the runtime implementation
+		if (std::find(tex.shared.begin(), tex.shared.end(), effect_index) == tex.shared.end())
+			continue; // Ignore textures not being used with this effect
 
 		std::filesystem::path source_path = std::filesystem::u8path(tex.annotation_as_string("source"));
 		// Ignore textures that have no image file attached to them (e.g. plain render targets)
@@ -3054,8 +3058,6 @@ void reshade::runtime::load_textures()
 
 		tex.loaded = true;
 	}
-
-	_textures_loaded = true;
 }
 bool reshade::runtime::create_texture(texture &tex)
 {
@@ -3492,7 +3494,6 @@ void reshade::runtime::destroy_effects()
 	assert(_textures.empty());
 	assert(_techniques.empty() && _technique_sorting.empty());
 
-	_textures_loaded = false;
 	_reload_required_effects.clear();
 }
 
@@ -3725,7 +3726,13 @@ void reshade::runtime::update_effects()
 	if (_reload_remaining_effects != std::numeric_limits<size_t>::max())
 		return;
 
-	if (!_reload_create_queue.empty())
+	if (_reload_create_queue.empty())
+	{
+#if RESHADE_ADDON
+		invoke_addon_event<addon_event::reshade_reloaded_effects>(this);
+#endif
+	}
+	else
 	{
 		// Pop an effect from the queue
 		const size_t effect_index = _reload_create_queue.back();
@@ -3748,9 +3755,6 @@ void reshade::runtime::update_effects()
 			_last_reload_successful = false;
 		}
 
-		// An effect has changed, need to reload textures
-		_textures_loaded = false;
-
 #if RESHADE_GUI
 		const effect &effect = _effects[effect_index];
 
@@ -3765,16 +3769,6 @@ void reshade::runtime::update_effects()
 			if (effect.assembly_text.find(instance.entry_point_name) != effect.assembly_text.end())
 				open_code_editor(instance);
 		}
-#endif
-	}
-
-	if (!_textures_loaded && _reload_create_queue.empty())
-	{
-		// Now that all effects were created, load all textures
-		load_textures();
-
-#if RESHADE_ADDON
-		invoke_addon_event<addon_event::reshade_reloaded_effects>(this);
 #endif
 	}
 }

source/runtime.hpp
@@ -61,7 +61,7 @@ namespace reshade
 		/// <summary>
 		/// Gets a boolean indicating whether effects are being loaded.
 		/// </summary>
-		bool is_loading() const { return _reload_remaining_effects != std::numeric_limits<size_t>::max() || !_reload_create_queue.empty() || (!_textures_loaded && _is_initialized); }
+		bool is_loading() const { return _reload_remaining_effects != std::numeric_limits<size_t>::max() || !_reload_create_queue.empty(); }
 #endif
 
 		void render_effects(api::command_list *cmd_list, api::resource_view rtv, api::resource_view rtv_srgb) final;
@@ -187,7 +187,7 @@ namespace reshade
 		bool create_effect_sampler_state(const reshadefx::sampler_desc &desc, api::sampler &sampler);
 		void destroy_effect(size_t effect_index);
 
-		void load_textures();
+		void load_textures(size_t effect_index);
 		bool create_texture(texture &texture);
 		void destroy_texture(texture &texture);
 
@@ -302,7 +302,6 @@ namespace reshade
 		std::vector<std::filesystem::path> _texture_search_paths;
 
 		std::atomic<bool> _last_reload_successful = true;
-		bool _textures_loaded = false;
 		std::shared_mutex _reload_mutex;
 		std::vector<size_t> _reload_create_queue;
 		std::atomic<size_t> _reload_remaining_effects = std::numeric_limits<size_t>::max();

commit 192f4d171383732503fcd5b94fd52322e5099a52

Fix "reshade_reloaded_effects" event being triggered every frame
This fixes a regression introduced by commit acf87102daaa9b077847d28eb7ef802a9e1c85ac

source/runtime.cpp
@@ -3723,54 +3723,50 @@ void reshade::runtime::update_effects()
 		return;
 	}
 
-	if (_reload_remaining_effects != std::numeric_limits<size_t>::max())
+	if (_reload_remaining_effects != std::numeric_limits<size_t>::max() || _reload_create_queue.empty())
 		return;
 
-	if (_reload_create_queue.empty())
+	// Pop an effect from the queue
+	const size_t effect_index = _reload_create_queue.back();
+	_reload_create_queue.pop_back();
+
+	if (!create_effect(effect_index))
 	{
-#if RESHADE_ADDON
-		invoke_addon_event<addon_event::reshade_reloaded_effects>(this);
-#endif
+		_graphics_queue->wait_idle();
+
+		// Destroy all textures belonging to this effect
+		for (texture &tex : _textures)
+			if (tex.effect_index == effect_index && tex.shared.size() <= 1)
+				destroy_texture(tex);
+		// Disable all techniques belonging to this effect
+		for (technique &tech : _techniques)
+			if (tech.effect_index == effect_index)
+				disable_technique(tech);
+
+		_effects[effect_index].compiled = false;
+		_last_reload_successful = false;
 	}
-	else
-	{
-		// Pop an effect from the queue
-		const size_t effect_index = _reload_create_queue.back();
-		_reload_create_queue.pop_back();
-
-		if (!create_effect(effect_index))
-		{
-			_graphics_queue->wait_idle();
-
-			// Destroy all textures belonging to this effect
-			for (texture &tex : _textures)
-				if (tex.effect_index == effect_index && tex.shared.size() <= 1)
-					destroy_texture(tex);
-			// Disable all techniques belonging to this effect
-			for (technique &tech : _techniques)
-				if (tech.effect_index == effect_index)
-					disable_technique(tech);
-
-			_effects[effect_index].compiled = false;
-			_last_reload_successful = false;
-		}
 
 #if RESHADE_GUI
-		const effect &effect = _effects[effect_index];
+	const effect &effect = _effects[effect_index];
 
-		// Update assembly in all code editors after a reload
-		for (editor_instance &instance : _editors)
-		{
-			if (!instance.generated || instance.entry_point_name.empty() || instance.file_path != effect.source_file)
-				continue;
+	// Update assembly in all code editors after a reload
+	for (editor_instance &instance : _editors)
+	{
+		if (!instance.generated || instance.entry_point_name.empty() || instance.file_path != effect.source_file)
+			continue;
 
-			assert(instance.effect_index == effect_index);
+		assert(instance.effect_index == effect_index);
 
-			if (effect.assembly_text.find(instance.entry_point_name) != effect.assembly_text.end())
-				open_code_editor(instance);
-		}
-#endif
+		if (effect.assembly_text.find(instance.entry_point_name) != effect.assembly_text.end())
+			open_code_editor(instance);
 	}
+#endif
+
+#if RESHADE_ADDON
+	if (_reload_create_queue.empty())
+		invoke_addon_event<addon_event::reshade_reloaded_effects>(this);
+#endif
 }
 void reshade::runtime::render_effects(api::command_list *cmd_list, api::resource_view rtv, api::resource_view rtv_srgb)
 {