0aef39b8254a73e67fb2553c3b0faad02b201713
Kaldaien <kaldaien2+github@gmail.com>
2024-11-27 15:20:50 -0500

    Fix formatting problems on reshadefs::type::t_float

--- source/runtime.cpp
+++ source/runtime.cpp
@@ -1842,7 +1842,7 @@ bool reshade::runtime::load_effect(const std::filesystem::path &source_file, con
 							break;
 						case reshadefx::type::t_float:
 							char temp[64];
-														const std::to_chars_result res = std::to_chars(temp, temp + sizeof(temp), constant.initializer_value.as_float[i]
+							const std::to_chars_result res = std::to_chars(temp, temp + sizeof(temp), constant.initializer_value.as_float[i]
 #if !defined(_HAS_COMPLETE_CHARCONV) || _HAS_COMPLETE_CHARCONV
 								, std::chars_format::scientific, 8
 #endif
