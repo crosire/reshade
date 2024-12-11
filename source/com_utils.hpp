d9d16a8294b03595300c2725b12ac9b34eea151a
crosire <crosiredev@gmail.com>
2024-12-11 22:04:41 +0100

    Improve object destruction tracking in D3D10/11/12 using "ID3DDestructionNotifier"

--- source/com_utils.hpp
+++ source/com_utils.hpp
@@ -31,49 +31,20 @@ __forceinline U *get_private_pointer_d3d9(T *object)
  /// Registers a <paramref name="callback"/> with the specified <paramref name="object"/> that is called when the object is being destroyed.
  /// </summary>
 template <typename T, typename L>
-inline void register_destruction_callback_d3dx(T *object, L &&callback, const UINT unique_tag = 0)
+inline void register_destruction_callback_d3dx(T *object, L &&callback)
 {
-	class tracker_instance final : public IUnknown
+	com_ptr<ID3DDestructionNotifier> notifier;
+	if (FAILED(object->QueryInterface(__uuidof(ID3DDestructionNotifier), reinterpret_cast<void **>(&notifier))))
 	{
-	public:
-		// The call to 'SetPrivateDataInterface' below increases the reference count, so set it to zero initially
-		tracker_instance(L &&callback) :
-			_ref(0), _callback(std::move(callback)) {}
-
-		ULONG STDMETHODCALLTYPE AddRef() override
-		{
-			return InterlockedIncrement(&_ref);
-		}
-		ULONG STDMETHODCALLTYPE Release() override
-		{
-			const ULONG ref = InterlockedDecrement(&_ref);
-			if (ref == 0)
-			{
-				_callback();
-				delete this;
-			}
-			return ref;
-		}
-
-		HRESULT STDMETHODCALLTYPE QueryInterface(REFIID, void **) override
-		{
-			return E_NOINTERFACE;
-		}
-
-	private:
-		ULONG _ref;
-		L const _callback;
-	};
-
-	const GUID private_guid = { 0xd6fe4f90 + unique_tag, 0x71b7, 0x473c, { 0xbe, 0x83, 0xea, 0x21, 0x9, 0x7a, 0xa3, 0xeb } };
-
-	// Check if a tracker instance was already registered with this object, in which case nothing needs to be done
-	if (UINT size = 0;
-		SUCCEEDED(object->GetPrivateData(private_guid, &size, nullptr)))
+		assert(false);
 		return;
+	}
 
-	IUnknown *const interface_object = new tracker_instance(std::move(callback));
-	object->SetPrivateDataInterface(private_guid, interface_object);
+	notifier->RegisterDestructionCallback(
+		[](void *data) {
+			(*static_cast<L *>(data))();
+			delete static_cast<L *>(data);
+		}, new L(std::move(callback)), nullptr);
 }
 template <typename T, typename L>
 inline void register_destruction_callback_d3d9(T *object, L &&callback, const UINT unique_tag = 0)
