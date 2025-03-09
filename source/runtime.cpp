crosire/reshade

commit a91d4b9807d478ca8a1ad56e40e11e19c9a9c86b

Fix crash after reloading effect with an editor open showing the assembly of a permutation

source/runtime.cpp
@@ -3789,7 +3789,7 @@ void reshade::runtime::update_effects()
 				// Set effect index again in case it was moved during the reload
 				instance.effect_index = std::distance(_effects.cbegin(), it);
 
-				if (instance.entry_point_name.empty())
+				if (instance.entry_point_name.empty() && instance.permutation_index < it->permutations.size())
 					open_code_editor(instance);
 				else
 					// Those editors referencing assembly will be updated in a separate step below
@@ -3834,7 +3834,9 @@ void reshade::runtime::update_effects()
 
 		assert(instance.effect_index == effect_index);
 
-		if (effect.permutations[permutation_index].assembly_text.find(instance.entry_point_name) != effect.permutations[permutation_index].assembly_text.end())
+		const effect::permutation &permutation = effect.permutations[permutation_index];
+
+		if (permutation.assembly_text.find(instance.entry_point_name) != permutation.assembly_text.end())
 			open_code_editor(instance);
 	}
 #endif

source/runtime_gui.cpp
@@ -4489,10 +4489,12 @@ void reshade::runtime::open_code_editor(editor_instance &instance) const
 
 	if (instance.generated)
 	{
+		const effect::permutation &permutation = effect.permutations[instance.permutation_index];
+
 		if (instance.entry_point_name.empty())
-			instance.editor.set_text(effect.permutations[instance.permutation_index].generated_code);
+			instance.editor.set_text(permutation.generated_code);
 		else
-			instance.editor.set_text(effect.permutations[instance.permutation_index].assembly_text.at(instance.entry_point_name));
+			instance.editor.set_text(permutation.assembly_text.at(instance.entry_point_name));
 		instance.editor.set_readonly(true);
 		return; // Errors only apply to the effect source, not generated code
 	}

commit 4ffa0e818b998e0357953a90e1caa60164243dee

Fix generated code editor always getting cleared on reload
This was a regression introduced in commit a91d4b9807d478ca8a1ad56e40e11e19c9a9c86b

source/runtime.cpp
@@ -3836,7 +3836,7 @@ void reshade::runtime::update_effects()
 				// Set effect index again in case it was moved during the reload
 				instance.effect_index = std::distance(_effects.cbegin(), it);
 
-				if (instance.entry_point_name.empty() && instance.permutation_index < it->permutations.size())
+				if (instance.entry_point_name.empty() && (instance.permutation_index < it->permutations.size() || !instance.generated))
 					open_code_editor(instance);
 				else
 					// Those editors referencing assembly will be updated in a separate step below