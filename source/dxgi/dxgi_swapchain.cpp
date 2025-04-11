9a6d13657e863cc8a30c44aa1244a7e908958ad6
crosire <crosiredev@gmail.com>
2025-04-11 23:09:41 +0200

    Fix effect runtime not being reset on the first DXGI color space update

--- source/dxgi/dxgi_swapchain.cpp
+++ source/dxgi/dxgi_swapchain.cpp
@@ -538,7 +538,7 @@ HRESULT STDMETHODCALLTYPE DXGISwapChain::SetColorSpace1(DXGI_COLOR_SPACE_TYPE Co
 #endif
 		reshade::log::message(reshade::log::level::info, "Redirecting IDXGISwapChain3::SetColorSpace1(ColorSpace = %d) ...", static_cast<int>(ColorSpace));
 
-	DXGI_COLOR_SPACE_TYPE prev_color_space = ColorSpace;
+	DXGI_COLOR_SPACE_TYPE prev_color_space = DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709;
 	UINT prev_color_space_size = sizeof(prev_color_space);
 	_orig->GetPrivateData(SKID_SwapChainColorSpace, &prev_color_space_size, &prev_color_space);
 
