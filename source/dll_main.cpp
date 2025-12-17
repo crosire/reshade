crosire/reshade

commit 769bad07e92307c7cf6afa64e15d9c7bb7ad4587

Fix crash in TRX - Tomb Raider I & II: Community Edition

source/dll_main.cpp
@@ -210,7 +210,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID)
 			// Check if another ReShade instance was already loaded into the process
 			if (HMODULE modules[1024]; K32EnumProcessModules(GetCurrentProcess(), modules, sizeof(modules), &fdwReason)) // Use kernel32 variant which is available in DllMain
 			{
-				for (DWORD i = 0; i < std::min<DWORD>(fdwReason / sizeof(HMODULE), ARRAYSIZE(modules)); ++i)
+				// Skip first module (the main application module)
+				for (DWORD i = 1; i < std::min<DWORD>(fdwReason / sizeof(HMODULE), ARRAYSIZE(modules)); ++i)
 				{
 					if (modules[i] != hModule && GetProcAddress(modules[i], "ReShadeVersion") != nullptr)
 					{

commit b8df2f817f6dcf115e2c67d10b1c6d5f730f6ae0

Avoid loading Vulkan layer in the ReShade test application
This was a regression introduced in commit 769bad07e92307c7cf6afa64e15d9c7bb7ad4587

source/dll_main.cpp
@@ -155,6 +155,20 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID)
 			// This e.g. prevents loading the implicit Vulkan layer when not explicitly enabled for an application
 			if (default_base_to_target_executable_path && !GetEnvironmentVariableW(L"RESHADE_DISABLE_LOADING_CHECK", nullptr, 0))
 			{
+#ifndef NDEBUG
+				// Avoid loading in the ReShade test application
+				if (g_target_executable_path.filename() ==
+#ifndef _WIN64
+						L"ReShade32.exe"
+#else
+						L"ReShade64.exe"
+#endif
+						)
+				{
+					return FALSE;
+				}
+#endif
+
 				std::error_code ec;
 				if (!std::filesystem::exists(config.path(), ec))
 				{