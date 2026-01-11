crosire/reshade

commit cbd1144056275faa8441a5c2a23839b8519decdd

Fix setup tool exception when configuration contains invalid search path

setup/MainWindow.xaml.cs
@@ -292,7 +292,22 @@ namespace ReShade.Setup
 			}
 
 			// Filter out invalid search paths (and those with remaining wildcards that were not handled above)
-			var validSearchPaths = searchPaths.Where(searchPath => searchPath.IndexOfAny(Path.GetInvalidPathChars()) < 0 && searchPath.IndexOf('*') < 0);
+			var validSearchPaths = searchPaths.Where(searchPath =>
+				{
+					if (searchPath.IndexOfAny(Path.GetInvalidPathChars()) < 0 && searchPath.IndexOf('*') < 0)
+					{
+						return false;
+					}
+					try
+					{
+						Path.GetFullPath(searchPath);
+						return true;
+					}
+					catch
+					{
+						return false;
+					}
+				});
 
 			// Avoid adding duplicate search paths (relative or absolute)
 			if (validSearchPaths.Any(searchPath => Path.GetFullPath(searchPath) == Path.GetFullPath(newPath)))
@@ -356,7 +371,22 @@ namespace ReShade.Setup
 				searchPaths = effectSearchPaths
 					.Where(searchPath => !string.IsNullOrWhiteSpace(searchPath))
 					.Select(searchPath => searchPath.EndsWith(wildcard) ? new KeyValuePair<string, bool>(searchPath.Remove(searchPath.Length - 1 - wildcard.Length), true) : new KeyValuePair<string, bool>(searchPath, false))
-					.Where(searchPath => searchPath.Key.IndexOfAny(Path.GetInvalidPathChars()) < 0 && searchPath.Key.IndexOf('*') < 0)
+					.Where(searchPath =>
+						{
+							if (searchPath.Key.IndexOfAny(Path.GetInvalidPathChars()) < 0 && searchPath.Key.IndexOf('*') < 0)
+							{
+								return false;
+							}
+							try
+							{
+								Path.GetFullPath(searchPath.Key);
+								return true;
+							}
+							catch
+							{
+								return false;
+							}
+						})
 					.Select(searchPath => new KeyValuePair<string, bool>(Path.GetFullPath(searchPath.Key), searchPath.Value))
 					.ToList();
 			}

commit f18dbd8e7989158dcd8635fd5367530565043621

Fix setup tool search path reading
This was a regression introduced in commit cbd1144056275faa8441a5c2a23839b8519decdd

setup/MainWindow.xaml.cs
@@ -294,7 +294,7 @@ namespace ReShade.Setup
 			// Filter out invalid search paths (and those with remaining wildcards that were not handled above)
 			var validSearchPaths = searchPaths.Where(searchPath =>
 				{
-					if (searchPath.IndexOfAny(Path.GetInvalidPathChars()) < 0 && searchPath.IndexOf('*') < 0)
+					if (searchPath.IndexOfAny(Path.GetInvalidPathChars()) >= 0 || searchPath.IndexOf('*') >= 0)
 					{
 						return false;
 					}
@@ -373,7 +373,7 @@ namespace ReShade.Setup
 					.Select(searchPath => searchPath.EndsWith(wildcard) ? new KeyValuePair<string, bool>(searchPath.Remove(searchPath.Length - 1 - wildcard.Length), true) : new KeyValuePair<string, bool>(searchPath, false))
 					.Where(searchPath =>
 						{
-							if (searchPath.Key.IndexOfAny(Path.GetInvalidPathChars()) < 0 && searchPath.Key.IndexOf('*') < 0)
+							if (searchPath.Key.IndexOfAny(Path.GetInvalidPathChars()) >= 0 || searchPath.Key.IndexOf('*') >= 0)
 							{
 								return false;
 							}