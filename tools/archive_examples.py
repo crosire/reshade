37085c98f1084bb30d1719708a81188779a66365
lobotomyx <lbatvt@gmail.com>
2026-03-04 00:26:33 -0500

    Delete tools/archive_examples.py

--- tools/archive_examples.py
+++
@@ -1,5 +0,0 @@
-from shutil import make_archive
-from os.path import join, getcwd, chdir
-chdir(".."))
-make_archive(f'Examples32.zip', 'zip', join(getcwd(), "bin", "Win32", "Release Examples"))
-make_archive(f'Examples64.zip', 'zip', join(getcwd(), "bin", "x64", "Release Examples"))
