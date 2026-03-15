8397a3be257f974ce7d21524066d9abaf6d28a7d
Mohanned Hassan <gailardiagalane@gmail.com>
2026-03-15 20:26:57 +0400

    Add support for dynamic uniform and storage buffer descriptors in Vulkan APIs

--- source/vulkan/vulkan_hooks_command_list.cpp
+++ source/vulkan/vulkan_hooks_command_list.cpp
@@ -1855,6 +1855,8 @@ void VKAPI_CALL vkCmdPushDescriptorSet(VkCommandBuffer commandBuffer, VkPipeline
 			break;
 		case VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER:
 		case VK_DESCRIPTOR_TYPE_STORAGE_BUFFER:
+		case VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC:
+		case VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC:
 			static_assert(sizeof(reshade::api::buffer_range) == sizeof(VkDescriptorBufferInfo));
 			update.descriptors = write.pBufferInfo;
 			break;
@@ -1944,6 +1946,8 @@ void VKAPI_CALL vkCmdPushDescriptorSetWithTemplate(VkCommandBuffer commandBuffer
 			break;
 		case VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER:
 		case VK_DESCRIPTOR_TYPE_STORAGE_BUFFER:
+		case VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC:
+		case VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC:
 			update.descriptors = static_cast<const VkDescriptorBufferInfo *>(base);
 			break;
 #if VK_KHR_acceleration_structure
